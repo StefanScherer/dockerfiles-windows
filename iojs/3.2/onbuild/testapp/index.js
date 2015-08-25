6c7081a78a46d2dde015696ee7048bd63392a786
Stefan Scherer <scherer_stefan@icloud.com>
2015-08-25 15:20:08 +0200

    Remove dependencies to simplify sample

--- iojs/3.2/onbuild/testapp/index.js
+++ iojs/3.2/onbuild/testapp/index.js
@@ -2,10 +2,7 @@
 
 const os = require('os');
 
-const randomstring = require("randomstring");
-
 console.log('Hello World from', os.hostname());
-console.log('Some random string', randomstring.generate());
 
 setTimeout(function () {
   console.log('Bye bye!');
