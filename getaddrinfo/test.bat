b009dc2acdbec79c7c26d7552c7d2c269fd090ae
Stefan Scherer <scherer_stefan@icloud.com>
2015-11-21 13:56:07 +0100

    Test with TP4

--- getaddrinfo/test.bat
+++ getaddrinfo/test.bat
@@ -1,2 +1,2 @@
-docker run -it getaddrinfo powershell.exe -Command "Sleep 5 ; .\getaddrinfo nodejs.org 0 0"
-docker run -it getaddrinfo powershell.exe -Command "Sleep 5 ; .\getaddrinfo nodejs.org 0 AI_ADDRCONFIG"
+docker run -t getaddrinfo powershell.exe -Command "Sleep 2 ; getaddrinfo.exe nodejs.org 0 0"
+docker run -t getaddrinfo powershell.exe -Command "Sleep 2 ; getaddrinfo.exe nodejs.org 0 AI_ADDRCONFIG"
