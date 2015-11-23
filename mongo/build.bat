f294960ccd780157e0ede0052fbac36ee25c899c
Stefan Scherer <scherer_stefan@icloud.com>
2015-11-23 22:00:16 +0100

    Update to Mongo 3.0.7

--- mongo/build.bat
+++ mongo/build.bat
@@ -1,2 +1,2 @@
 docker build -t mongo 3.0
-docker tag mongo:latest mongo:3.0.6
+docker tag mongo:latest mongo:3.0.7
--- mongo/build.sh
+++ mongo/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t mongo 3.0
-docker tag mongo:latest mongo:3.0.6
+docker tag mongo:latest mongo:3.0.7
