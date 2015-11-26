85e0e0de72167758d1b9978a51547be109de28e0
Stefan Scherer <scherer_stefan@icloud.com>
2015-11-26 21:52:16 +0100

    Build node:5.1.0

--- node/build.bat
+++ node/build.bat
@@ -2,3 +2,8 @@ docker build -t node 4.2 || exit /b
 docker tag -f node:latest node:4.2.1 || exit /b
 
 docker build -t node:onbuild 4.2/onbuild
+
+docker build -t node 5.1 || exit /b
+docker tag -f node:latest node:5.1.0 || exit /b
+
+docker build -t node:onbuild 5.1/onbuild
