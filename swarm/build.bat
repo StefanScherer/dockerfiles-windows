14e1d56160b8ad4eb1d7e387c02c914ba42ce9d3
Stefan Scherer <scherer_stefan@icloud.com>
2015-11-29 20:50:57 +0100

    Add swarm 1.0.0

---
+++ swarm/build.bat
@@ -0,0 +1,5 @@
+docker build -t swarm-builder build
+docker rm -f swarm-builder
+docker run --name swarm-builder swarm-builder swarm --version
+docker cp swarm-builder:/go/bin/swarm.exe ./swarm.exe
+docker build -t swarm .
