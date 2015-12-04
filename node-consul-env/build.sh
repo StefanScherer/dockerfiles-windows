2b9af4f7b9c9b709563177b3957db2c1d7f9840a
Stefan Scherer <scherer_stefan@icloud.com>
2015-12-04 16:33:20 +0100

    First running version

--- node-consul-env/build.sh
+++ node-consul-env/build.sh
@@ -1,3 +1,3 @@
  #!/bin/bash
  docker build -t node-consul-env .
- docker tag node-consul-env:latest node-consul-env:4.2.1-0.5.2-0.6.0
+ docker tag -f node-consul-env:latest node-consul-env:4.2.1-0.5.2-0.6.0
--- node-consul-env/run.cmd
+++ node-consul-env/run.cmd
@@ -1,9 +1,9 @@
 set prefixes_any=-prefix dc/home/environment/service/any/tag/any/
 set prefixes_service=-prefix dc/home/environment/service/%SERVICE_NAME%/tag/any/
 
-start consul agent -config-dir=/config
+start \consul.exe agent -config-dir=/config
 if "%1" == "cmd" (
   %*
 ) else (
-  envconsul -consul %CONSUL_HOST%:8500 -sanitize -upcase %prefixes_any% %prefixes_service% node %*
+  \envconsul.exe -consul %CONSUL_HOST%:8500 -sanitize -upcase %prefixes_any% %prefixes_service% node %*
 )
