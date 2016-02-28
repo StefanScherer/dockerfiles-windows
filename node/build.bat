4478c8015256d904384604aa0788254f778a93c0
Stefan Scherer <scherer_stefan@icloud.com>
2016-02-28 10:46:13 +0100

    Update node 5.7.0

--- node/build.bat
+++ node/build.bat
@@ -3,7 +3,7 @@ docker tag node:latest node:4.3.1 || exit /b
 
 docker build -t node:onbuild 4.3/onbuild
 
-docker build -t node 5.5 || exit /b
-docker tag node:latest node:5.5.0 || exit /b
+docker build -t node 5.7 || exit /b
+docker tag node:latest node:5.7.0 || exit /b
 
-docker build -t node:onbuild 5.5/onbuild
+docker build -t node:onbuild 5.7/onbuild
