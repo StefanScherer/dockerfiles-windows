3dcaf31cdd357cc4ef504b668a49b7699f05302d
Stefan Scherer <scherer_stefan@icloud.com>
2016-02-28 10:45:27 +0100

    Update node 4.3.1

--- node/build.bat
+++ node/build.bat
@@ -1,7 +1,7 @@
-docker build -t node 4.2 || exit /b
-docker tag node:latest node:4.2.6 || exit /b
+docker build -t node 4.3 || exit /b
+docker tag node:latest node:4.3.1 || exit /b
 
-docker build -t node:onbuild 4.2/onbuild
+docker build -t node:onbuild 4.3/onbuild
 
 docker build -t node 5.5 || exit /b
 docker tag node:latest node:5.5.0 || exit /b
