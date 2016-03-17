dbe7bdb9c9594bb267ce36531109d58c120a3385
Stefan Scherer <scherer_stefan@icloud.com>
2016-03-17 23:49:17 +0100

    Update Node.js 4.4.0

--- node/build.bat
+++ node/build.bat
@@ -1,9 +1,9 @@
-docker build -t node 4.3 || exit /b
-docker tag node:latest node:4.3.1 || exit /b
+docker build -t node 4.4 || exit /b
+docker tag node:latest node:4.4.0 || exit /b
 
-docker build -t node:onbuild 4.3/onbuild
+docker build -t node:onbuild 4.4/onbuild
 
-docker build -t node 5.7 || exit /b
-docker tag node:latest node:5.7.0 || exit /b
+docker build -t node 5.9 || exit /b
+docker tag node:latest node:5.9.0 || exit /b
 
-docker build -t node:onbuild 5.7/onbuild
+docker build -t node:onbuild 5.9/onbuild
--- node/build.sh
+++ node/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t node 4.2
-docker tag node:latest node:4.2.1
+docker build -t node 4.4
+docker tag node:latest node:4.4.0
 
-docker build -t node:onbuild 4.2/onbuild
+docker build -t node:onbuild 4.4/onbuild
