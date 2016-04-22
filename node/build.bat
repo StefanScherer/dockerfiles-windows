561f4757c766250d1823a5e78d367863c0470426
Stefan Scherer <scherer_stefan@icloud.com>
2016-04-22 07:24:12 +0200

    Update Node 5.11.0

--- node/build.bat
+++ node/build.bat
@@ -1,9 +1,9 @@
 docker build -t node 4.4 || exit /b
-docker tag node:latest node:4.4.1 || exit /b
+docker tag node:latest node:4.4.3 || exit /b
 
 docker build -t node:onbuild 4.4/onbuild
 
-docker build -t node 5.10 || exit /b
-docker tag node:latest node:5.10.1 || exit /b
+docker build -t node 5.11 || exit /b
+docker tag node:latest node:5.11.0 || exit /b
 
-docker build -t node:onbuild 5.10/onbuild
+docker build -t node:onbuild 5.11/onbuild
--- node/build.sh
+++ node/build.sh
@@ -4,3 +4,8 @@ docker build -t node 4.4
 docker tag node:latest node:4.4.1
 
 docker build -t node:onbuild 4.4/onbuild
+
+docker build -t node 5.11
+docker tag node:latest node:5.11.0
+
+docker build -t node:onbuild 5.11/onbuild
