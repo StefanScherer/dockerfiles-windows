44b4c6e82c09d75b0d7a5f50c6b009f7ec9c5eef
Stefan Scherer <scherer_stefan@icloud.com>
2016-08-18 14:50:13 +0200

    Update Node.js 6.4.0
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- node/build.bat
+++ node/build.bat
@@ -7,11 +7,11 @@ docker build -t node:4.5.0-onbuild 4.5/onbuild
 docker tag node:4.5.0-onbuild node:4.5-onbuild || exit /b
 docker tag node:4.5.0-onbuild node:4-onbuild || exit /b
 
-REM docker build -t node:6.4.0 6.4 || exit /b
-REM docker tag node:6.4.0 node:latest || exit /b
-REM docker tag node:6.4.0 node:6.4 || exit /b
-REM docker tag node:6.4.0 node:6 || exit /b
-REM
-REM docker build -t node:6.4.0-onbuild 6.4/onbuild
-REM docker tag node:6.4.0-onbuild node:6.4-onbuild || exit /b
-REM docker tag node:6.4.0-onbuild node:6-onbuild || exit /b
+docker build -t node:6.4.0 6.4 || exit /b
+docker tag node:6.4.0 node:latest || exit /b
+docker tag node:6.4.0 node:6.4 || exit /b
+docker tag node:6.4.0 node:6 || exit /b
+
+docker build -t node:6.4.0-onbuild 6.4/onbuild
+docker tag node:6.4.0-onbuild node:6.4-onbuild || exit /b
+docker tag node:6.4.0-onbuild node:6-onbuild || exit /b
--- node/push.bat
+++ node/push.bat
@@ -6,16 +6,16 @@ docker tag node:4.5.0-onbuild stefanscherer/node-windows:4.5.0-onbuild || exit /
 docker tag node:4.5-onbuild stefanscherer/node-windows:4.5-onbuild || exit /b
 docker tag node:4-onbuild stefanscherer/node-windows:4-onbuild || exit /b
 
-REM docker tag node:6.4.0 stefanscherer/node-windows:6.4.0 || exit /b
-REM docker tag node:6.4 stefanscherer/node-windows:6.4 || exit /b
-REM docker tag node:6 stefanscherer/node-windows:6 || exit /b
-REM
-REM docker tag node:6.4.0-onbuild stefanscherer/node-windows:6.4.0-onbuild || exit /b
-REM docker tag node:6.4-onbuild stefanscherer/node-windows:6.4-onbuild || exit /b
-REM docker tag node:6-onbuild stefanscherer/node-windows:6-onbuild || exit /b
-REM
-REM docker tag node:6 stefanscherer/node-windows:latest || exit /b
-REM docker tag node:6-onbuild stefanscherer/node-windows:onbuild || exit /b
+docker tag node:6.4.0 stefanscherer/node-windows:6.4.0 || exit /b
+docker tag node:6.4 stefanscherer/node-windows:6.4 || exit /b
+docker tag node:6 stefanscherer/node-windows:6 || exit /b
+
+docker tag node:6.4.0-onbuild stefanscherer/node-windows:6.4.0-onbuild || exit /b
+docker tag node:6.4-onbuild stefanscherer/node-windows:6.4-onbuild || exit /b
+docker tag node:6-onbuild stefanscherer/node-windows:6-onbuild || exit /b
+
+docker tag node:6 stefanscherer/node-windows:latest || exit /b
+docker tag node:6-onbuild stefanscherer/node-windows:onbuild || exit /b
 
 docker push stefanscherer/node-windows:4.5.0
 docker push stefanscherer/node-windows:4.5
@@ -24,11 +24,11 @@ docker push stefanscherer/node-windows:4.5.0-onbuild
 docker push stefanscherer/node-windows:4.5-onbuild
 docker push stefanscherer/node-windows:4-onbuild
 
-REM docker push stefanscherer/node-windows:6.4.0
-REM docker push stefanscherer/node-windows:6.4
-REM docker push stefanscherer/node-windows:6
-REM docker push stefanscherer/node-windows:6.4.0-onbuild
-REM docker push stefanscherer/node-windows:6.4-onbuild
-REM docker push stefanscherer/node-windows:6-onbuild
-REM docker push stefanscherer/node-windows:onbuild
-REM docker push stefanscherer/node-windows:latest
+docker push stefanscherer/node-windows:6.4.0
+docker push stefanscherer/node-windows:6.4
+docker push stefanscherer/node-windows:6
+docker push stefanscherer/node-windows:6.4.0-onbuild
+docker push stefanscherer/node-windows:6.4-onbuild
+docker push stefanscherer/node-windows:6-onbuild
+docker push stefanscherer/node-windows:onbuild
+docker push stefanscherer/node-windows:latest
