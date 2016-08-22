8e9c7ebcf6f519cabd5046b000a9dd717d2587e5
Stefan Scherer <scherer_stefan@icloud.com>
2016-08-22 03:35:17 -0700

    Add Node.js 6.4.0 nanoserver image
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

---
+++ node/6.4/nano/push.bat
@@ -0,0 +1,14 @@
+docker tag node:6.4.0-nano stefanscherer/node-windows:6.4.0-nano || exit /b
+docker tag node:6.4-nano stefanscherer/node-windows:6.4-nano || exit /b
+docker tag node:4-nano stefanscherer/node-windows:4-nano || exit /b
+
+docker tag node:6.4.0-nano-onbuild stefanscherer/node-windows:6.4.0-nano-onbuild || exit /b
+docker tag node:6.4-nano-onbuild stefanscherer/node-windows:6.4-nano-onbuild || exit /b
+docker tag node:4-nano-onbuild stefanscherer/node-windows:4-nano-onbuild || exit /b
+
+docker push stefanscherer/node-windows:6.4.0-nano
+docker push stefanscherer/node-windows:6.4-nano
+docker push stefanscherer/node-windows:4-nano
+docker push stefanscherer/node-windows:6.4.0-nano-onbuild
+docker push stefanscherer/node-windows:6.4-nano-onbuild
+docker push stefanscherer/node-windows:4-nano-onbuild
