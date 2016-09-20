2281d09c4fe66106762ef241452dd1a177436403
Stefan Scherer <scherer_stefan@icloud.com>
2016-09-20 07:22:32 +0200

    Create tag windowsservercore
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- docker/build.bat
+++ docker/build.bat
@@ -13,6 +13,8 @@ if not "%1x"=="x" (
   git checkout pr%1
   set image=dockerpr%1
 )
+rem workaround from https://github.com/docker/docker/pull/26722
+docker tag microsoft/windowsservercore:latest windowsservercore:latest
 docker build -t %image% -f Dockerfile.windows .
 docker run --rm -v "%BINDIR%:c:\target" %image% sh -c 'cd /c/go/src/github.com/docker/docker; hack/make.sh binary; ec=$?; if [ $ec -eq 0 ]; then robocopy /c/go/src/github.com/docker/docker/bundles/$(cat VERSION)/binary-client /c/target/binary; robocopy /c/go/src/github.com/docker/docker/bundles/$(cat VERSION)/binary-daemon /c/target/binary; fi; exit $ec'
 popd
