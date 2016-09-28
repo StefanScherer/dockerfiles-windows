db13e7d981ec4f9247aa851cd3e1cc24b1768939
Stefan Scherer <scherer_stefan@icloud.com>
2016-09-28 10:04:04 -0700

    Update MongoDB 3.2.9

--- mongo/build.bat
+++ mongo/build.bat
@@ -2,5 +2,5 @@ pushd 3.2
 docker build -t mongo-builder -f Dockerfile.build .
 docker run -v %cd%:C:\share mongo-builder
 docker build -t mongo .
-docker tag mongo:latest mongo:3.2.6
+docker tag mongo:latest mongo:3.2.9
 popd
---
+++ mongo/push.bat
@@ -0,0 +1,5 @@
+docker tag mongo:3.2.9 stefanscherer/mongo-windows:3.2.9
+docker tag mongo:3.2.9 stefanscherer/mongo-windows:latest
+
+docker push stefanscherer/mongo-windows:3.2.9
+docker push stefanscherer/mongo-windows:latest
