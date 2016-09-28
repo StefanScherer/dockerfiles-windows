74604bd14c5e5f29b0f8d3df31a978ee61d5a475
Stefan Scherer <scherer_stefan@icloud.com>
2016-09-28 10:39:49 -0700

    Playing with docker/docker#25987

--- compose/app.js
+++ compose/app.js
@@ -1,14 +1,25 @@
 'use strict';
 
-const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dns = require('dns');
 
-const sleep = 30;
+const MongoClient = require('mongodb').MongoClient;
+
+const sleep = 10;
 console.log('Sleeping',sleep,"seconds before connecting to MongoDB...");
+
+var host = "compose_mongo_1";
+
+setTimeout(() => {
+  dns.lookup(host, (err, address, family) => {
+    console.log('Lookup ' + host + ': ' + address);
+    host = address;
+  });
+}, 2000);
 
 setTimeout(() => {
   // Connection URL
-  var url = 'mongodb://mongo:27017/myproject';
+  var url = 'mongodb://compose_mongo_1:27017/myproject';
   // Use connect method to connect to the Server
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
