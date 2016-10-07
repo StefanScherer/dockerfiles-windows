00fad36354945ee2e40dde8c141911e0eacf21c0
Stefan Scherer <scherer_stefan@icloud.com>
2016-10-07 23:59:38 +0200

    Update registry 2.5.1

--- registry/build.sh
+++ registry/build.sh
@@ -1,8 +1,10 @@
-if not exist registry.exe (
+#!/bin/bash
+
+if [ ! -e registry.exe ]; then
   docker build -t registry-builder build
   docker rm -f registry-builder
   docker run --name registry-builder registry-builder registry --version
   docker cp registry-builder:/go/src/github.com/docker/distribution/registry.exe ./registry.exe
-)
+fi
 docker build -t registry .
 docker tag registry:latest registry:2.5.1
