1c857046158fea5ee5aa0588cd6b3856ba84a00b
Stefan Scherer <scherer_stefan@icloud.com>
2016-10-23 07:45:43 -0700

    Create server and client certs

--- dockertls/README.md
+++ dockertls/README.md
@@ -5,20 +5,21 @@ The script https://github.com/Microsoft/Virtualization-Documentation/blob/master
 
 ## Usage
 
+## Test drive
+
+Just run it in a clean environment creating two folders on your host:
+
 ```
-mkdir certs
-docker run -e SERVER_NAME=$(hostname) -e IP_ADDRESSES="192.168.254.135 127.0.0.1" -v "$(pwd)\certs:c:\certs" dockertls
-dir certs
-
-    Directory: C:\Users\demo\certs
-
-Mode                LastWriteTime         Length Name
-----                -------------         ------ ----
--a----       10/19/2016   2:26 PM           3380 ca-key.pem
--a----       10/19/2016   2:26 PM           1976 ca.pem
--a----       10/19/2016   2:26 PM           1810 cert.pem
--a----       10/19/2016   2:26 PM           1822 server-cert.pem
--a----       10/19/2016   2:26 PM           3294 server-key.pem
+mkdir server
+mkdir client\.docker
+docker run `
+  -e SERVER_NAME=$(hostname) `
+  -e IP_ADDRESSES=127.0.0.1,192.168.254.135 `
+  -v "$(pwd)\server:c:\programdata\docker" `
+  -v "$(pwd)\client\.docker:c:\users\containeradministrator\.docker" dockertls
+dir server\certs.d
+dir server\config
+dir client\.docker
 ```
 
 
--- dockertls/generate-certs.ps1
+++ dockertls/generate-certs.ps1
@@ -1,7 +1,55 @@
 $ErrorActionPreference = "Stop"
 
-function createCerts($certsPath) {
-  Write-Host "TODO"
+function ensureDirs($dirs) {
+  foreach ($dir in $dirs) {
+    if (!(Test-Path $dir)) {
+      mkdir $dir
+    }
+  }
+}
+
+# https://docs.docker.com/engine/security/https/
+function createCerts($certsPath, $serverName, $ipAddresses) {
+  $caPass = "pass:$(openssl rand -base64 32)"
+
+  Write-Host "`n=== Generating CA private key"
+  & openssl genrsa -aes256 -passout $caPass -out ca-key.pem 4096
+
+  Write-Host "`n=== Generating CA public key"
+  & openssl req -subj "/C=US/ST=Washington/L=Redmond/O=./OU=." -new -x509 -days 365 -passin $caPass -key ca-key.pem -sha256 -out ca.pem
+
+  Write-Host "`n=== Generating CA private key"
+  & openssl genrsa -out server-key.pem 4096
+
+  Write-Host "`n=== Generating Server signing request"
+  & openssl req -subj "/CN=$serverName/" -sha256 -new -key server-key.pem -out server.csr
+
+  Write-Host "`n=== Signing Server request"
+  "subjectAltName = " + (($ipAddresses.Split(' ') | ForEach-Object { "IP:$_" }) -join ',') | Out-File extfile.cnf -Encoding Ascii
+  cat extfile.cnf
+  & openssl x509 -req -days 365 -sha256 -in server.csr -CA ca.pem -passin $caPass -CAkey ca-key.pem `
+    -CAcreateserial -out server-cert.pem -extfile extfile.cnf
+
+  Write-Host "`n=== Generating Client key"
+  & openssl genrsa -out key.pem 4096
+
+  Write-Host "`n=== Generating Client signing request"
+  & openssl req -subj '/CN=client' -new -key key.pem -out client.csr
+
+  Write-Host "`n=== Signing Client signing request"
+  "extendedKeyUsage = clientAuth" | Out-File extfile.cnf -Encoding Ascii
+  & openssl x509 -req -days 365 -sha256 -in client.csr -CA ca.pem -passin $caPass -CAkey ca-key.pem `
+    -CAcreateserial -out cert.pem -extfile extfile.cnf
+
+  Write-Host "`n=== Copying Server certifcates to $certPath"
+  copy ca.pem $certsPath\ca.pem
+  copy server-cert.pem $certsPath\server-cert.pem
+  copy server-key.pem $certsPath\server-key.pem
+
+  Write-Host "`n=== Copying Client certifcates to $userPath"
+  copy ca.pem $userPath\ca.pem
+  copy cert.pem $userPath\cert.pem
+  copy key.pem $userPath\key.pem
 }
 
 function updateConfig($daemonJson, $certsPath) {
@@ -18,8 +66,19 @@ function updateConfig($daemonJson, $certsPath) {
     tlskey = "$certsPath\server-key.pem" `
     }) -Force -PassThru
 
+  Write-Host "`n=== Creating / Updating $daemonJson"
   $config | ConvertTo-Json | Set-Content $daemonJson -Encoding Ascii
 }
 
-createCerts "C:\ProgramData\docker\certs.d"
-updateConfig "C:\ProgramData\docker\config\daemon.json" "C:\ProgramData\docker\certs.d"
+$dockerData = "$env:ProgramData\docker"
+$serverName = $env:SERVER_NAME
+$ipAddresses = $env:IP_ADDRESSES
+$userPath = "$env:USERPROFILE\.docker"
+
+ensureDirs @("$dockerData\certs.d", "$dockerData\config", "$userPath")
+createCerts "$dockerData\certs.d" $serverName $ipAddresses
+updateConfig "$dockerData\config\daemon.json" "$dockerData\certs.d" "$userPath"
+
+Write-Host "`n=== Finished"
+Write-Host "Now restart Docker service with the following command:"
+Write-Host "restart-service docker"
