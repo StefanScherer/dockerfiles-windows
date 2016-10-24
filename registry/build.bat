9d8bc5922a669e458a2aeea46fc9aaea03285c9d
Tobias Gesellchen <tobias@gesellix.de>
2016-10-24 23:13:23 +0200

    don't run the builder image when we only want to extract the registry binary from it

--- registry/build.bat
+++ registry/build.bat
@@ -1,7 +1,7 @@
 if not exist registry.exe (
   docker build -t registry-builder build
   docker rm -f registry-builder
-  docker run --name registry-builder registry-builder /go/src/github.com/docker/distribution/registry.exe --version
+  docker create --name registry-builder registry-builder
   docker cp registry-builder:/go/src/github.com/docker/distribution/registry.exe ./registry.exe
 )
 docker build -t registry .
