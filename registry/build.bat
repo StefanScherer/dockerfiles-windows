874dd7eb412de9708c8272e6d98af52879854b77
Tobias Gesellchen <tobias@gesellix.de>
2016-10-24 20:49:00 +0200

    work around some strange error while fetching tools/godep

--- registry/build.bat
+++ registry/build.bat
@@ -1,7 +1,7 @@
 if not exist registry.exe (
   docker build -t registry-builder build
   docker rm -f registry-builder
-  docker run --name registry-builder registry-builder registry --version
+  docker run --name registry-builder registry-builder /go/src/github.com/docker/distribution/registry.exe --version
   docker cp registry-builder:/go/src/github.com/docker/distribution/registry.exe ./registry.exe
 )
 docker build -t registry .
