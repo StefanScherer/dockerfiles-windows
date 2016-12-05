ec37cb8ddfc6e7760d35078c4a16258b06a11528
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-05 09:26:40 +0100

    Tag LibreSSL version number

--- dockertls/push.bat
+++ dockertls/push.bat
@@ -1,2 +1,4 @@
+docker tag dockertls stefanscherer/dockertls-windows:2.5.0
 docker tag dockertls stefanscherer/dockertls-windows
+docker push stefanscherer/dockertls-windows:2.5.0
 docker push stefanscherer/dockertls-windows
