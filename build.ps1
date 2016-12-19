d44edc05df0de01277226308c04cafff3b691d99
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-19 08:45:06 +0100

    Simplify

--- build.ps1
+++ build.ps1
@@ -8,7 +8,7 @@ docker pull microsoft/nanoserver
 
 Write-Host Removing old images
 $ErrorActionPreference = 'SilentlyContinue';
-docker rmi $(docker images --no-trunc --format '{{.Repository}}:{{.Tag}}' | sls -notmatch -pattern '(REPOSITORY|microsoft\/(windowsservercore|nanoserver))' | Foreach {($_ -split '\s+',4)[0]})
+docker rmi $(docker images --no-trunc --format '{{.Repository}}:{{.Tag}}' | sls -notmatch -pattern '(REPOSITORY|microsoft\/(windowsservercore|nanoserver))')
 $ErrorActionPreference = 'Stop';
 Write-Host Prune system
 docker system prune -f
