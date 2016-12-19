9673c118eb209a7ab16eb1d5ccfbb6a468364bcf
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-19 17:12:25 +0100

    Update openjdk 1.8.0.111-3.b15
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

---
+++ java/openjdk/ojdkbuild/push.bat
@@ -0,0 +1,4 @@
+docker tag openjdk:8-windowsservercore stefanscherer/openjdk-windows:8-windowsservercore
+docker tag openjdk:8-windowsservercore stefanscherer/openjdk-windows:8u111-windowsservercore
+docker tag openjdk:8-nanoserver stefanscherer/openjdk-windows:8-nanoserver
+docker tag openjdk:8-nanoserver stefanscherer/openjdk-windows:8u111-nanoserver
