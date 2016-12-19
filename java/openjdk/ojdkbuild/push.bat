04b2a6c30f83cd68454f16f89802d657a504cf9d
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-19 19:06:14 +0100

    Push java images

--- java/openjdk/ojdkbuild/push.bat
+++ java/openjdk/ojdkbuild/push.bat
@@ -2,3 +2,8 @@ docker tag openjdk:8-windowsservercore stefanscherer/openjdk-windows:8-windowsse
 docker tag openjdk:8-windowsservercore stefanscherer/openjdk-windows:8u111-windowsservercore
 docker tag openjdk:8-nanoserver stefanscherer/openjdk-windows:8-nanoserver
 docker tag openjdk:8-nanoserver stefanscherer/openjdk-windows:8u111-nanoserver
+
+docker push stefanscherer/openjdk-windows:8-windowsservercore
+docker push stefanscherer/openjdk-windows:8u111-windowsservercore
+docker push stefanscherer/openjdk-windows:8-nanoserver
+docker push stefanscherer/openjdk-windows:8u111-nanoserver
