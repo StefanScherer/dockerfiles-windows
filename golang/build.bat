947952132ac208ce94f265db1d1e55b7705de661
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-20 18:07:20 +0100

    Update golang 1.8beta2, git 2.11.0
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- golang/build.bat
+++ golang/build.bat
@@ -1,3 +1,3 @@
 docker build -t golang .
 if %errorlevel% neq 0 exit /b %errorlevel%
-docker tag golang:latest golang:1.8beta1
+docker tag golang:latest golang:1.8beta2
---
+++ golang/push.bat
@@ -0,0 +1,4 @@
+docker tag dockertls stefanscherer/golang-windows:1.8beta2
+docker tag dockertls stefanscherer/golang-windows
+docker push stefanscherer/golang-windows:1.8beta2
+docker push stefanscherer/golang-windows
