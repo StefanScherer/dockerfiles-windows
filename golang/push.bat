a9ef0b6e6a3209331eeea47f3502acfb6e624740
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-20 19:00:38 +0100

    Fix docker image

--- golang/push.bat
+++ golang/push.bat
@@ -1,4 +1,4 @@
-docker tag dockertls stefanscherer/golang-windows:1.8beta2
-docker tag dockertls stefanscherer/golang-windows
+docker tag golang:1.8beta2 stefanscherer/golang-windows:1.8beta2
+docker tag golang:1.8beta2 stefanscherer/golang-windows
 docker push stefanscherer/golang-windows:1.8beta2
 docker push stefanscherer/golang-windows
