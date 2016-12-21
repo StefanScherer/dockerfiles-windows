ab860842c33afe82c96930576af60d1e81ef3728
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-21 09:31:50 +0100

    Satisfy AppVeyor

--- mongo/3.4/build.bat
+++ mongo/3.4/build.bat
@@ -1,14 +1,12 @@
 docker build -t mongobuilder -f Dockerfile.build .
-docker rm -f mongobuilder
 docker create --name=mongobuilder mongobuilder
-rm -rf build
 mkdir build
 
 docker cp mongobuilder:/mongodb build
 docker cp mongobuilder:/windows/system32/msvcp140.dll build/mongodb/bin
 docker cp mongobuilder:/windows/system32/msvcr140.dll build/mongodb/bin
 docker cp mongobuilder:/windows/system32/vcruntime140.dll build/mongodb/bin
-rm -f build/mongodb/bin/*.pdb
+del build\mongodb\bin\*.pdb
 
 docker build -t mongo .
 docker tag mongo:latest mongo:3.4.1
