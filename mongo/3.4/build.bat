289a6762b7f814d09c67da94debfa538763b15e1
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-21 12:11:31 +0100

    Update MongoDB 3.4.1 (#48)
    * Update MongoDB 3.4.1
    
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>
    
    * Satisfy AppVeyor
    
    * Copy only two runtime DLL's
    
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>
    
    * Remove bash comment
    
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- mongo/3.4/build.bat
+++ mongo/3.4/build.bat
@@ -1,15 +1,11 @@
-#!/bin/bash
-set -e
 docker build -t mongobuilder -f Dockerfile.build .
-docker rm -f mongobuilder
 docker create --name=mongobuilder mongobuilder
-rm -rf build
 mkdir build
 
 docker cp mongobuilder:/mongodb build
 docker cp mongobuilder:/windows/system32/msvcp140.dll build/mongodb/bin
 docker cp mongobuilder:/windows/system32/vcruntime140.dll build/mongodb/bin
-rm -f build/mongodb/bin/*.pdb
+del build\mongodb\bin\*.pdb
 
 docker build -t mongo .
 docker tag mongo:latest mongo:3.4.1
@@ -17,7 +13,6 @@ docker tag mongo:latest mongo:3.4.1
 docker build -t mongo:nano -f nano/Dockerfile .
 docker tag mongo:nano mongo:3.4.1-nano
 
-# check images
 echo "Check windowsservercore"
 docker run mongo mongod.exe -version
 echo "Check nanoserver"
