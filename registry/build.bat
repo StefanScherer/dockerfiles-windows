d3bf701534e41bfdafce3f029c8fa0c879fb84cf
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-31 11:13:14 +0100

    Build registry 2.6.0-rc.2
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- registry/build.bat
+++ registry/build.bat
@@ -1,8 +1,8 @@
-if not exist registry.exe (
-  docker build -t registry-builder build
-  docker rm -f registry-builder
-  docker create --name registry-builder registry-builder
-  docker cp registry-builder:/gopath/src/github.com/docker/distribution/registry.exe ./registry.exe
-)
+echo Building registry binary
+docker build -t registry-builder build
+docker create --name registry-builder registry-builder
+docker cp registry-builder:/gopath/src/github.com/docker/distribution/registry.exe ./registry.exe
+
+echo Building registry image
 docker build -t registry .
-docker tag registry:latest registry:2.6.0-rc.1
+docker tag registry:latest registry:2.6.0-rc.2
--- registry/push.bat
+++ registry/push.bat
@@ -1,4 +1,4 @@
-docker tag registry:2.6.0-rc.1 stefanscherer/registry-windows:2.6.0-rc.1
-docker tag registry:2.6.0-rc.1 stefanscherer/registry-windows:latest
-docker push stefanscherer/registry-windows:2.6.0-rc.1
+docker tag registry:2.6.0-rc.2 stefanscherer/registry-windows:2.6.0-rc.2
+docker tag registry:2.6.0-rc.2 stefanscherer/registry-windows:latest
+docker push stefanscherer/registry-windows:2.6.0-rc.2
 docker push stefanscherer/registry-windows:latest
