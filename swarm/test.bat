c87aae11a4ff2f938c55a801d484efcbd17f3ce3
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-03 15:54:57 +0100

    Add tests, update Docker swarm 1.2.6-rc1
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

---
+++ swarm/test.bat
@@ -0,0 +1,5 @@
+docker run swarm:1.2.6-rc1 --help
+docker run swarm:1.2.6-rc1 create
+
+docker run swarm:1.2.6-rc1-nano --help
+docker run swarm:1.2.6-rc1-nano create
---
+++ test.ps1
@@ -0,0 +1,39 @@
+$ErrorActionPreference = 'Stop';
+$files = ""
+Write-Host Starting test
+
+if ( $env:APPVEYOR_PULL_REQUEST_NUMBER ) {
+  Write-Host Pull request $env:APPVEYOR_PULL_REQUEST_NUMBER
+  $files = $(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master))
+} else {
+  Write-Host Branch $env:APPVEYOR_REPO_BRANCH
+  $files = $(git diff --name-only HEAD~1)
+}
+
+Write-Host Changed files:
+
+$dirs = @{}
+
+$files | ForEach-Object {
+  Write-Host $_
+  $dir = $_ -replace "\/[^\/]+$", ""
+  $dir = $dir -replace "/", "\"
+  if (Test-Path "$dir\test.bat") {
+    Write-Host "Storing $dir for test"
+    $dirs.Set_Item($dir, 1)
+  } else {
+    $dir = $dir -replace "\\[^\\]+$", ""
+    if (Test-Path "$dir\test.bat") {
+      Write-Host "Storing $dir for test"
+      $dirs.Set_Item($dir, 1)
+    }
+  }
+}
+
+$dirs.GetEnumerator() | Sort-Object Name | ForEach-Object {
+  $dir = $_.Name
+  Write-Host Testing in directory $dir
+  pushd $dir
+  . .\test.bat
+  popd
+}
