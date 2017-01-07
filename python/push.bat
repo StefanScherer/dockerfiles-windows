260bf6baf3d61604614a36da1d573882c7d3606c
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-07 01:54:27 +0100

    Add build
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

---
+++ python/push.bat
@@ -0,0 +1,4 @@
+docker tag python stefanscherer/python-windows:3.6.0
+docker tag python stefanscherer/python-windows
+docker push stefanscherer/python-windows:3.6.0
+docker push stefanscherer/python-windows
