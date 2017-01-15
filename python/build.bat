33f13d9cb0245928657fa9fe2fcc7b09d2610b63
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-15 15:09:44 +0100

    Pull python base servercore image

--- python/build.bat
+++ python/build.bat
@@ -1,6 +1,7 @@
 rem docker build -t python .
 
 docker rm -f pythonbuild36 2>&1
+docker pull python:3.6-windowsservercore
 docker create --name=pythonbuild36 python:3.6-windowsservercore
 docker cp pythonbuild36:Python tmp
 docker build -t python -f Dockerfile.build .
