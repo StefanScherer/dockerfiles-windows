24a79f667268d09190600016b124b85d75a97d65
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-16 20:49:13 +0100

    Build nanoserverapiscan image with AppVeyor
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

---
+++ nanoserverapiscan/push.bat
@@ -0,0 +1,2 @@
+docker tag nanoserverapiscan stefanscherer/nanoserverapiscan:latest
+docker tag nanoserverapiscan:onbuild stefanscherer/nanoserverapiscan:onbuild
---
+++ nanoserverapiscan/test.bat
@@ -0,0 +1,1 @@
+docker run --rm nanoserverapiscan C:\tool\NanoServerApiScan.exe -h
