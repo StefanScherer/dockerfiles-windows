e5ebe36e1a701538fed85fbf46d9907c62437b39
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-16 20:18:12 +0100

    Port from ps1 to bat
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- dockertls/test.bat
+++ dockertls/test.bat
@@ -1,10 +1,6 @@
 mkdir server
 mkdir client\.docker
-docker run --rm `
-  -e SERVER_NAME=test `
-  -e IP_ADDRESSES=127.0.0.1 `
-  -v "%cd%\server:c:\programdata\docker" `
-  -v "%cd%\client\.docker:c:\users\containeradministrator\.docker" dockertls
+docker run --rm -e SERVER_NAME=test -e IP_ADDRESSES=127.0.0.1 -v "%cd%\server:c:\programdata\docker" -v "%cd%\client\.docker:c:\users\containeradministrator\.docker" dockertls
 dir server\certs.d
 dir server\config
 dir client\.docker
