d2173f5538b8b03bf71123741025dd06ca9f05b0
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-17 06:20:13 +0100

    Redirect to stdout

--- node/test.bat
+++ node/test.bat
@@ -6,6 +6,6 @@ goto :eof
 :test
 setlocal
 docker run node:%1 node --version
-docker run node:%1 npm.cmd --version
+docker run node:%1 npm.cmd --version 2>&1
 docker run node:%1-nano node --version
-docker run node:%1-nano npm.cmd --version
+docker run node:%1-nano npm.cmd --version 2>&1
