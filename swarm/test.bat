a0b43996b0534dc49e649959d053288151f450ab
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-18 07:05:02 +0100

    Update swarm 1.2.6
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- swarm/test.bat
+++ swarm/test.bat
@@ -1,5 +1,5 @@
-docker run swarm:1.2.6-rc1 --help
-docker run swarm:1.2.6-rc1 create
+docker run swarm:1.2.6 --help
+docker run swarm:1.2.6 create
 
-docker run swarm:1.2.6-rc1-nano --help
-docker run swarm:1.2.6-rc1-nano create
+docker run swarm:1.2.6-nano --help
+docker run swarm:1.2.6-nano create
