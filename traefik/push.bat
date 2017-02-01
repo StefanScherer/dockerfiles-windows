b19bcea27ce010bb3f20c082edc4b7a350ca5f6a
Stefan Scherer <scherer_stefan@icloud.com>
2017-02-01 22:31:19 +0100

    Use binary from Travis build to fix UI
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- traefik/push.bat
+++ traefik/push.bat
@@ -1,2 +1,4 @@
 docker tag traefik stefanscherer/traefik-windows
+docker tag traefik stefanscherer/traefik-windows:v1.1.2-windows.1
+docker push stefanscherer/traefik-windows:v1.1.2-windows.1
 docker push stefanscherer/traefik-windows
