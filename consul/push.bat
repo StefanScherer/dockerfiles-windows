981d3c8948231f9676ee1a81638df220552d563d
Stefan Scherer <scherer_stefan@icloud.com>
2017-02-22 08:06:43 +0100

    Update Consul 0.7.5

--- consul/push.bat
+++ consul/push.bat
@@ -1,4 +1,4 @@
-docker tag consul:0.7.4 stefanscherer/consul-windows:0.7.4 || exit /b
-docker tag consul:0.7.4 stefanscherer/consul-windows:latest || exit /b
-docker push stefanscherer/consul-windows:0.7.4
+docker tag consul:0.7.5 stefanscherer/consul-windows:0.7.5 || exit /b
+docker tag consul:0.7.5 stefanscherer/consul-windows:latest || exit /b
+docker push stefanscherer/consul-windows:0.7.5
 docker push stefanscherer/consul-windows:latest
