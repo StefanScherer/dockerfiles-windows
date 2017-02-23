0cf4e72607b58be98e982e1ff53b702b242381c6
Stefan Scherer <scherer_stefan@icloud.com>
2017-02-23 08:33:45 +0100

    Move build steps to build.bat

--- webserver/build.bat
+++ webserver/build.bat
@@ -1,2 +1,7 @@
+docker build -t webserverbuild -f Dockerfile.build .
+docker rm -f webserverbuild
+docker create --name webserverbuild webserverbuild
+mkdir tmp
+docker cp webserverbuild:/code/webserver.exe tmp
 docker build -t webserver .
 docker tag webserver:latest webserver:0.2.0
