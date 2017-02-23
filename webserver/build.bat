b42ba4e2cee980f7a3f17e118369ff805cfcbba3
Stefan Scherer <scherer_stefan@icloud.com>
2017-02-23 08:28:58 +0100

    Build, test, push

---
+++ webserver/build.bat
@@ -0,0 +1,2 @@
+docker build -t webserver .
+docker tag webserver:latest webserver:0.2.0
---
+++ webserver/test.bat
@@ -0,0 +1,3 @@
+docker run --name webserver -p 8080:8080 -d webserver:0.2.0
+Start-Sleep 5
+docker logs webserver
