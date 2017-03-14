e7e08b11d5c5f54bfb285d454d2fea5c59c97d2f
Stefan Scherer <scherer_stefan@icloud.com>
2017-03-14 17:33:30 +0100

    Build locally

---
+++ docker/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+if [ ! -d docker  ]; then 
+  git clone https://github.com/docker/docker
+fi
+
+if [ ! -d binary  ]; then
+  mkdir binary
+fi
+
+BINDIR=$(pwd)
+image=docker
+pushd docker
+if [ $1 != ""  ]; then
+  git fetch origin pull/$1/head:pr$1
+  git checkout pr$1
+  image=dockerpr$1
+fi
+docker rm -f $image
+docker build -t $image -f Dockerfile.windows .
+docker run --name $image $image 'hack\make.ps1' -Binary
+docker cp $image:C:/go/src/github.com/docker/docker/bundles/docker.exe ../binary/docker.exe
+docker cp $image:C:/go/src/github.com/docker/docker/bundles/dockerd.exe ../binary/dockerd.exe
+popd
+ls -l binary
