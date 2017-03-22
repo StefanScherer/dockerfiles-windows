935ba76134ac1378b5261d867752bb0073f9a675
Stefan Scherer <scherer_stefan@icloud.com>
2017-03-22 07:34:28 +0100

    Sync Dockerfiles with upstream pull requests

--- node/build.bat
+++ node/build.bat
@@ -9,6 +9,7 @@ docker build -t node:%1 %2 || exit /b
 docker tag node:%1 node:latest || exit /b
 docker tag node:%1 node:%2 || exit /b
 docker tag node:%1 node:%3 || exit /b
+docker tag node:%1 node:%3-windowsservercore || exit /b
 
 docker build -t node:%1-onbuild %2/onbuild
 docker tag node:%1-onbuild node:%2-onbuild || exit /b
@@ -18,6 +19,7 @@ docker build -t node:%1-nano %2/nano || exit /b
 docker tag node:%1-nano node:latest-nano || exit /b
 docker tag node:%1-nano node:%2-nano || exit /b
 docker tag node:%1-nano node:%3-nano || exit /b
+docker tag node:%1-nano node:%3-nanoserver || exit /b
 
 docker build -t node:%1-nano-onbuild %2/nano/onbuild || exit /b
 docker tag node:%1-nano-onbuild node:%2-nano-onbuild || exit /b
