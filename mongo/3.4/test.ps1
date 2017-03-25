6cfa74b10bfb669fa9ae67de198cc756f3c3562e
Stefan Scherer <scherer_stefan@icloud.com>
2017-03-25 12:35:41 +0100

    Push to Docker Hub

--- mongo/3.4/test.ps1
+++ mongo/3.4/test.ps1
@@ -2,4 +2,5 @@ Write-Host "Check nanoserver"
 docker run mongo:nano mongod.exe -version
 
 Write-Host "Pushing image to Docker Hub"
+docker login -u="$env:DOCKER_USER" -p="$env:DOCKER_PASS"
 .\push.ps1
