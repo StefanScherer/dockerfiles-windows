e5a5ee96f2152f443753909e09da44ceade2737c
Stefan Scherer <scherer_stefan@icloud.com>
2017-03-25 10:49:00 +0100

    Fix script

--- consul/push.ps1
+++ consul/push.ps1
@@ -1,4 +1,4 @@
-docker tag consul:0.7.5 stefanscherer/consul-windows:0.7.5 || exit /b
-docker tag consul:0.7.5 stefanscherer/consul-windows:latest || exit /b
+docker tag consul:0.7.5 stefanscherer/consul-windows:0.7.5
+docker tag consul:0.7.5 stefanscherer/consul-windows:latest
 docker push stefanscherer/consul-windows:0.7.5
 docker push stefanscherer/consul-windows:latest
