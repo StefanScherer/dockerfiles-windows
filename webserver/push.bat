faa563838584ed830a1904d1bccc7e0e22535e54
Stefan Scherer <scherer_stefan@icloud.com>
2017-03-25 10:49:42 +0100

    Switch to ps1

--- webserver/push.bat
+++
@@ -1,4 +0,0 @@
-docker tag webserver:0.2.0 stefanscherer/webserver-windows:0.2.0 || exit /b
-docker tag webserver:0.2.0 stefanscherer/webserver-windows:latest || exit /b
-docker push stefanscherer/webserver-windows:0.2.0
-docker push stefanscherer/webserver-windows:latest
---
+++ webserver/push.ps1
@@ -0,0 +1,4 @@
+docker tag webserver:0.2.0 stefanscherer/webserver-windows:0.2.0
+docker tag webserver:0.2.0 stefanscherer/webserver-windows:latest
+docker push stefanscherer/webserver-windows:0.2.0
+docker push stefanscherer/webserver-windows:latest
