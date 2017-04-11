2d7f6d4216ed40494331ed20e59bcc053d483a77
Stefan Scherer <scherer_stefan@icloud.com>
2017-04-11 23:25:37 +0200

    Update Python 3.6.1 and base image 10.0.14393.1066

--- python/push.ps1
+++ python/push.ps1
@@ -1,4 +1,4 @@
-docker tag python stefanscherer/python-windows:3.6.0-nano
+docker tag python stefanscherer/python-windows:3.6.1-nano
 docker tag python stefanscherer/python-windows:nano
-docker push stefanscherer/python-windows:3.6.0-nano
+docker push stefanscherer/python-windows:3.6.1-nano
 docker push stefanscherer/python-windows:nano
