31df3904c3f9957f4dbd61b57d7a443136cb8289
Stefan Scherer <scherer_stefan@icloud.com>
2017-04-23 19:48:48 +0200

    Fix local tag name

--- caddy/push.ps1
+++ caddy/push.ps1
@@ -1,4 +1,4 @@
-docker tag webserver stefanscherer/caddy-windows:0.10
-docker tag webserver stefanscherer/caddy-windows:latest
+docker tag caddy:0.10 stefanscherer/caddy-windows:0.10
+docker tag caddy:0.10 stefanscherer/caddy-windows:latest
 docker push stefanscherer/caddy-windows:0.10
 docker push stefanscherer/caddy-windows:latest
