0529da08109a18281a5bc53e39843f408beeca52
pennywisdom <alex.harrington@oneadvanced.com>
2017-04-24 07:46:52 +0100

    updating README and tests

--- caddy/test.ps1
+++ caddy/test.ps1
@@ -1,3 +1,10 @@
 docker run --name caddy -d -p 8080:80 -v "$(pwd)\conf:C:\caddy\conf" -v "$(pwd)\www:C:\wwwroot" caddy:0.10
 Start-Sleep -Seconds 5
+$req = Invoke-WebRequest http://$(docker inspect -f '{{ .NetworkSettings.Networks.nat.IPAddress }}' caddy) -UseBasicParsing
+$code = $req.statuscode
+$content = $req.content
+if($code -ne 200 -or !($content.Contains('Hello Docker from Caddy'))){
+  throw "Caddy test failed!"
+}
+echo "$content"
 docker logs caddy
