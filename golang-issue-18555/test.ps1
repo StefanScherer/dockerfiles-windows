d89815d9c2c418e2e74cbeb2aa215ad7e3c7815f
Stefan Scherer <scherer_stefan@icloud.com>
2017-04-30 18:14:37 +0200

    Add more tests

--- golang-issue-18555/test.ps1
+++ golang-issue-18555/test.ps1
@@ -1,2 +1,10 @@
 Write-Host Testing new golang image to build on volume mountpoint
+Write-Host The webserver dir before running the go build:
+dir ..\webserver
 docker run -v "$(pwd)\..\webserver:C:\code" -w /code golang-issue-18555 go build webserver.go
+Write-Host Current dir after running the go build:
+Write-Host The webserver dir after running the go build:
+dir ..\webserver
+if (!(Test-Path ..\webserver\webserver.exe)) {
+  Write-Error webserver.exe is missing, go build didn't work in container
+}
