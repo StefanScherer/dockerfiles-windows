26670e53a88c36aa4dad197c3e800d90d7073a62
Stefan Scherer <scherer_stefan@icloud.com>
2017-05-05 07:55:47 +0200

    Fix container name

--- prometheus/test.ps1
+++ prometheus/test.ps1
@@ -1,4 +1,4 @@
-docker run --name caddy -d -p 9090:9090 prometheus
+docker run --name prometheus -d -p 9090:9090 prometheus
 Start-Sleep -Seconds 5
 $req = Invoke-WebRequest http://$(docker inspect -f '{{ .NetworkSettings.Networks.nat.IPAddress }}' prometheus):9090/graph -UseBasicParsing
 $code = $req.statuscode
