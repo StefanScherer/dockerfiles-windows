937f95cac0e43d59f011f477e9e2f5627d8d7097
Stefan Scherer <scherer_stefan@icloud.com>
2017-05-19 08:11:29 +0200

    Push latest and nano tags

--- diskspd/push.ps1
+++ diskspd/push.ps1
@@ -1,5 +1,9 @@
 $version=$(select-string -Path Dockerfile -Pattern "ENV DISKSPD_VERSION").ToString().split()[-1]
 docker tag diskspd stefanscherer/diskspd:$version
 docker tag diskspd:nano stefanscherer/diskspd:$version-nano
+docker tag diskspd stefanscherer/diskspd:latest
+docker tag diskspd:nano stefanscherer/diskspd:nano
 docker push stefanscherer/diskspd:$version
 docker push stefanscherer/diskspd:$version-nano
+docker push stefanscherer/diskspd:latest
+docker push stefanscherer/diskspd:nano
