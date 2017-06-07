e760d43d4c759fc852c17dbbb9b6896c6dfcf5a6
Stefan Scherer <scherer_stefan@icloud.com>
2017-06-07 11:47:45 +0200

    Build chocolatey 10.6.1

--- chocolatey/push.ps1
+++ chocolatey/push.ps1
@@ -1,5 +1,5 @@
-docker tag chocolatey:0.10.3 stefanscherer/chocolatey:0.10.3
-docker tag chocolatey:0.10.3 stefanscherer/chocolatey:latest
+docker tag chocolatey:0.10.6.1 stefanscherer/chocolatey:0.10.6.1
+docker tag chocolatey:0.10.6.1 stefanscherer/chocolatey:latest
 
-docker push stefanscherer/chocolatey:0.10.3
+docker push stefanscherer/chocolatey:0.10.6.1
 docker push stefanscherer/chocolatey:latest
