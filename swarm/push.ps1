007b93347cf6f01283dc7a9d48346fa92860de6e
Stefan Scherer <scherer_stefan@icloud.com>
2017-07-01 08:44:27 +0200

    Update swarm 1.2.7

--- swarm/push.ps1
+++ swarm/push.ps1
@@ -1,11 +1,5 @@
-docker tag swarm:1.2.6 stefanscherer/swarm-windows:1.2.6
+docker tag swarm:1.2.7 stefanscherer/swarm-windows:1.2.7
 docker tag swarm:latest stefanscherer/swarm-windows:latest
 
-docker tag swarm:1.2.6-nano stefanscherer/swarm-windows:1.2.6-nano
-docker tag swarm:1.2.6-nano swarm:latest-nano
-docker tag swarm:latest-nano stefanscherer/swarm-windows:latest-nano
-
-docker push stefanscherer/swarm-windows:1.2.6
+docker push stefanscherer/swarm-windows:1.2.7
 docker push stefanscherer/swarm-windows:latest
-docker push stefanscherer/swarm-windows:1.2.6-nano
-docker push stefanscherer/swarm-windows:latest-nano
