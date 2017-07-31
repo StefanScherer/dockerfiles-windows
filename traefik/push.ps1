17d5204796723781b5f5625527aac7186b28e01b
Stefan Scherer <scherer_stefan@icloud.com>
2017-07-31 22:51:42 +0200

    Update Traefik 1.3.4

--- traefik/push.ps1
+++ traefik/push.ps1
@@ -1,4 +1,5 @@
+$version=$(select-string -Path Dockerfile -Pattern "ENV PROMETHEUS_VERSION").ToString().split()[-1]
 docker tag traefik stefanscherer/traefik-windows
-docker tag traefik stefanscherer/traefik-windows:v1.3.1
-docker push stefanscherer/traefik-windows:v1.3.1
+docker tag traefik stefanscherer/traefik-windows:v1.3.4
+docker push stefanscherer/traefik-windows:v1.3.4
 docker push stefanscherer/traefik-windows
