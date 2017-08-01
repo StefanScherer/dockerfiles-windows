f0731701f3682d996d09eea03eb298c9696e4df7
Stefan Scherer <scherer_stefan@icloud.com>
2017-08-01 08:11:01 +0200

    Fix version number

--- traefik/push.ps1
+++ traefik/push.ps1
@@ -1,5 +1,5 @@
-$version=$(select-string -Path Dockerfile -Pattern "ENV PROMETHEUS_VERSION").ToString().split()[-1]
+$version=$(select-string -Path Dockerfile -Pattern "ENV TRAEFIK_VERSION").ToString().split()[-1]
 docker tag traefik stefanscherer/traefik-windows
-docker tag traefik stefanscherer/traefik-windows:v1.3.4
-docker push stefanscherer/traefik-windows:v1.3.4
+docker tag traefik stefanscherer/traefik-windows:v$version
+docker push stefanscherer/traefik-windows:v$version
 docker push stefanscherer/traefik-windows
