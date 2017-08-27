047143a2d61e4e67263e57559be32bd181b063fa
Stefan Scherer <scherer_stefan@icloud.com>
2017-08-27 18:57:36 +0200

    Use Docker 17.06.1-ee

--- update-docker-ce.ps1
+++ update-docker-ce.ps1
@@ -1,21 +1,7 @@
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
-$version = "17.06.0-ce"
-if ($(docker --version).StartsWith("Docker version $version")) {
-  Write-Host Already updated.
-  exit 0;
-}
-
-Write-Host "Stopping docker service"
-Stop-Service docker
-
-Write-Host "Downloading docker-$version.zip"
-$wc = New-Object net.webclient
-$wc.DownloadFile("https://download.docker.com/win/static/edge/x86_64/docker-$version.zip", "$env:TEMP\docker-$version.zip")
-Write-Host "Extracting docker-$version.zip"
-Expand-Archive -Path "$env:TEMP\docker-$version.zip" -DestinationPath $env:ProgramFiles -Force
-Remove-Item "$env:TEMP\docker-$version.zip"
+Install-Package -Name docker -ProviderName DockerMsftProviderInsider -Verbose -Update -Force
 
 Write-Host "Starting docker service"
 Start-Service docker
