dd9078c743f959f14b5f45d4579d65b38d1b264d
Stefan Scherer <scherer_stefan@icloud.com>
2017-09-28 22:14:03 +0200

    Skip devel test

--- golang/test.ps1
+++ golang/test.ps1
@@ -1,7 +1,3 @@
 Write-Host Testing golang image
 docker run golang go version
 docker run golang git --version
-
-Write-Host Testing golang:devel image
-docker run golang:devel go version
-docker run golang:devel git --version
