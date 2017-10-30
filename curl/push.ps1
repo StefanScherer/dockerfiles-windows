dc15a6d02120ced094b3a1052234cd816b73dc16
Stefan Scherer <scherer_stefan@icloud.com>
2017-10-30 12:48:56 +0100

    Create multi-os curl image

--- curl/push.ps1
+++ curl/push.ps1
@@ -1,9 +1,9 @@
 $version=$(select-string -Path Dockerfile -Pattern "ENV CURL_VERSION").ToString().split()[-1]
-docker tag curl stefanscherer/curl-windows
-docker tag curl stefanscherer/curl-windows:$version
-docker push stefanscherer/curl-windows:$version
-docker push stefanscherer/curl-windows
+docker tag curl stefanscherer/curl-windows:$version-2016
+docker push stefanscherer/curl-windows:$version-2016
 
 npm install -g rebase-docker-image
-rebase-docker-image stefanscherer/curl-windows:$version -t stefanscherer/curl-windows:$version-1709 -b microsoft/nanoserver:1709
-rebase-docker-image stefanscherer/curl-windows:$version -t stefanscherer/curl-windows:1709 -b microsoft/nanoserver:1709
+rebase-docker-image stefanscherer/curl-windows:$version-2016 -t stefanscherer/curl-windows:$version-1709 -b microsoft/nanoserver:1709
+
+choco install -y manifest-tool
+manifest-tool push from-spec manifest.yml
