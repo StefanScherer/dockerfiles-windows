8f9f2b9593dc02e8cd4fa36901313ac8724fc9f0
Stefan Scherer <scherer_stefan@icloud.com>
2017-10-30 11:41:26 +0100

    Add more tests

--- busybox/test.ps1
+++ busybox/test.ps1
@@ -1,3 +1,4 @@
-docker run busybox wget --help
-docker run busybox curl --help
-docker run busybox ls -l
+docker run -v "$(pwd):C:/dl" -w C:/dl busybox wget -O docker.zip https://download.docker.com/win/static/edge/x86_64/docker-17.10.0-ce.zip
+docker run -v "$(pwd):C:/dl" -w C:/dl busybox unzip docker.zip
+docker run -v "$(pwd):C:/dl" -w C:/dl busybox curl -o docker-with-curl.zip https://download.docker.com/win/static/edge/x86_64/docker-17.10.0-ce.zip
+docker run -v "$(pwd):C:/dl" -w C:/dl busybox ls
