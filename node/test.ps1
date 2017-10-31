cf9b4e7f0668b492ec95698b14bf07915fd9aa9e
Stefan Scherer <scherer_stefan@icloud.com>
2017-10-31 16:02:48 +0100

    Improve tests

--- node/test.ps1
+++ node/test.ps1
@@ -1,7 +1,7 @@
 function testCommand($image, $command, $expected) {
-  Write-Host "Testing docker image $image with command $command"
+  Write-Host "Testing docker image $image with command $command --version"
   $ErrorActionPreference = 'SilentlyContinue';
-  $actual = $(docker run $image $command)
+  $actual = $(docker run $image $command --version)
   $ErrorActionPreference = 'Stop';
   if ($LastExitCode) {
     Write-Error "Command exited with error $LastExitCode"
@@ -15,11 +15,11 @@ function testCommand($image, $command, $expected) {
 }
 
 function testVersion($majorMinorPatch) {
-  testCommand "node:$majorMinorPatch-windowsservercore" "node --version" "v$majorMinorPatch"
-  testCommand "node:$majorMinorPatch-windowsservercore" "npm.cmd --version" ""
+  testCommand "node:$majorMinorPatch-windowsservercore" "node" "v$majorMinorPatch"
+  testCommand "node:$majorMinorPatch-windowsservercore" "npm.cmd" ""
 
-  testCommand "node:$majorMinorPatch-nanoserver" "node --version" "v$majorMinorPatch"
-  testCommand "node:$majorMinorPatch-nanoserver" "npm.cmd --version" ""
+  testCommand "node:$majorMinorPatch-nanoserver" "node" "v$majorMinorPatch"
+  testCommand "node:$majorMinorPatch-nanoserver" "npm.cmd" ""
 }
 
 testVersion "6.11.5"
