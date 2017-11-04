828ea4a8412acf81a4afb3e150ed1bf0703c4823
Stefan Scherer <scherer_stefan@icloud.com>
2017-11-04 16:09:37 +0100

    Use docker manifest command

--- curl/push.ps1
+++ curl/push.ps1
@@ -5,5 +5,16 @@ docker push stefanscherer/curl-windows:$version-2016
 npm install -g rebase-docker-image
 rebase-docker-image stefanscherer/curl-windows:$version-2016 -t stefanscherer/curl-windows:$version-1709 -b microsoft/nanoserver:1709
 
-choco install -y manifest-tool
-manifest-tool push from-spec manifest.yml
+Invoke-WebRequest -UseBasicParsing https://6582-88013053-gh.circle-artifacts.com/0/work/build/docker-windows-amd64 -OutFile docker.exe
+
+.\docker.exe manifest create `
+  stefanscherer/curl-windows:$version `
+  stefanscherer/curl-windows:$version-2016 `
+  stefanscherer/curl-windows:$version-1709
+.\docker.exe manifest push stefanscherer/curl-windows:$version
+
+.\docker.exe manifest create `
+  stefanscherer/curl-windows:latest `
+  stefanscherer/curl-windows:$version-2016 `
+  stefanscherer/curl-windows:$version-1709
+.\docker.exe manifest push stefanscherer/curl-windows:latest
