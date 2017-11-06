e3c1130015d52377fee2eb43d88f4411ec053bfa
Stefan Scherer <scherer_stefan@icloud.com>
2017-11-06 07:59:47 +0100

    Formatting

--- consul/push.ps1
+++ consul/push.ps1
@@ -3,9 +3,14 @@ docker tag consul stefanscherer/consul-windows:$version-2016
 docker push stefanscherer/consul-windows:$version-2016
 
 npm install -g rebase-docker-image
-rebase-docker-image stefanscherer/consul-windows:$version-2016 -t stefanscherer/consul-windows:$version-1709 -b microsoft/nanoserver:1709
+rebase-docker-image `
+  stefanscherer/consul-windows:$version-2016 `
+  -t stefanscherer/consul-windows:$version-1709 `
+  -b microsoft/nanoserver:1709
 
-Invoke-WebRequest -UseBasicParsing https://6582-88013053-gh.circle-artifacts.com/0/work/build/docker-windows-amd64 -OutFile docker.exe
+Invoke-WebRequest -UseBasicParsing `
+  https://6582-88013053-gh.circle-artifacts.com/0/work/build/docker-windows-amd64 `
+  -OutFile docker.exe
 
 .\docker.exe manifest create `
   stefanscherer/consul-windows:$version `
