6a94f80edbfa5094855a21d7da6a0224948d0b89
Stefan Scherer <scherer_stefan@icloud.com>
2018-01-07 15:27:31 +0100

    Set ErrorAction

--- busybox/test.ps1
+++ busybox/test.ps1
@@ -1,4 +1,4 @@
-# $ErrorActionPreference = 'SilentlyContinue';
+$ErrorActionPreference = 'SilentlyContinue';
 Write-Output "test curl"
 docker run -v "$(pwd):C:/dl" -w C:/dl busybox curl -o docker.zip https://download.docker.com/win/static/edge/x86_64/docker-17.10.0-ce.zip
 
@@ -10,7 +10,7 @@ docker run -v "$(pwd):C:/dl" -w C:/dl busybox unzip docker.zip
 
 Write-Output "test ls unzipped folder"
 docker run -v "$(pwd):C:/dl" -w C:/dl busybox ls docker
-# $ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 
 Write-Output "test ls unzipped folder on host"
 ls docker
