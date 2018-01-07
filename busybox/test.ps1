00bfa66c59863aa5e7fa1f1c5f75bab72bedb0b7
Stefan Scherer <scherer_stefan@icloud.com>
2018-01-07 15:05:06 +0100

    Add output

--- busybox/test.ps1
+++ busybox/test.ps1
@@ -1,7 +1,16 @@
-$ErrorActionPreference = 'SilentlyContinue';
+# $ErrorActionPreference = 'SilentlyContinue';
+Write-Output test wget
 docker run -v "$(pwd):C:/dl" -w C:/dl busybox wget http://google.com
+
+Write-Output test curl
 docker run -v "$(pwd):C:/dl" -w C:/dl busybox curl -o docker.zip https://download.docker.com/win/static/edge/x86_64/docker-17.10.0-ce.zip
+
+Write-Output test unzip
 docker run -v "$(pwd):C:/dl" -w C:/dl busybox unzip docker.zip
+
+Write-Output test ls unzipped folder
 docker run -v "$(pwd):C:/dl" -w C:/dl busybox ls docker
-$ErrorActionPreference = 'Stop';
+# $ErrorActionPreference = 'Stop';
+
+Write-Output test ls unzipped folder on host
 ls docker
