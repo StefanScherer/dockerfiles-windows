a6f2bafb529b19c67da6b261369019fc59387382
Stefan Scherer <scherer_stefan@icloud.com>
2018-01-07 13:58:59 +0100

    Fix tag

--- ruby/push.ps1
+++ ruby/push.ps1
@@ -1,5 +1,5 @@
 $version=$(select-string -Path Dockerfile -Pattern "ENV RUBY_VERSION").ToString().split()[-1]
 docker tag ruby stefanscherer/ruby-windows:$version
 docker push stefanscherer/ruby-windows:$version
-docker push stefanscherer/ruby-windows:latest
+docker tag ruby stefanscherer/ruby-windows:latest
 docker push stefanscherer/ruby-windows:latest
