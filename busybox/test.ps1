c6a73de1a47f39aeb2ced63e45f52e65572557a0
Stefan Scherer <scherer_stefan@icloud.com>
2018-01-07 15:07:47 +0100

    Add quotes

--- busybox/test.ps1
+++ busybox/test.ps1
@@ -1,16 +1,16 @@
 # $ErrorActionPreference = 'SilentlyContinue';
-Write-Output test wget
-docker run -v "$(pwd):C:/dl" -w C:/dl busybox wget http://google.com
-
-Write-Output test curl
+Write-Output "test curl"
 docker run -v "$(pwd):C:/dl" -w C:/dl busybox curl -o docker.zip https://download.docker.com/win/static/edge/x86_64/docker-17.10.0-ce.zip
 
-Write-Output test unzip
+Write-Output "test wget"
+docker run -v "$(pwd):C:/dl" -w C:/dl busybox wget https://download.docker.com/win/static/edge/x86_64/docker-17.10.0-ce.zip
+
+Write-Output "test unzip"
 docker run -v "$(pwd):C:/dl" -w C:/dl busybox unzip docker.zip
 
-Write-Output test ls unzipped folder
+Write-Output "test ls unzipped folder"
 docker run -v "$(pwd):C:/dl" -w C:/dl busybox ls docker
 # $ErrorActionPreference = 'Stop';
 
-Write-Output test ls unzipped folder on host
+Write-Output "test ls unzipped folder on host"
 ls docker
