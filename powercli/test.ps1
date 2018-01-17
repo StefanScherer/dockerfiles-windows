d95127c88ef2bdf9c64d0b82235dead756cc1223
Stefan Scherer <scherer_stefan@icloud.com>
2018-01-17 12:26:19 +0100

    Fix test

--- powercli/test.ps1
+++ powercli/test.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'SilentlyContinue';
 out=$(docker run powercli)
-if(!($content.Contains('Welcome to VMware PowerCLI!')) {
+if(!($content.Contains('Welcome to VMware PowerCLI!'))) {
   throw "PowerCLI test failed!"
 }
