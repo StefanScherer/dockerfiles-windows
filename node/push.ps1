55a0817f4544120d824e3098d42d73e57e34ec43
Stefan Scherer <scherer_stefan@icloud.com>
2018-02-01 07:18:01 +0100

    Update Node.js 9.5.0

--- node/push.ps1
+++ node/push.ps1
@@ -56,5 +56,5 @@ npm install -g rebase-docker-image
 choco install -y manifest-tool
 
 pushVersion "6.12.3" "6.12" "6"
-pushVersion "9.4.0" "9.4" "9"
-pushVersion "8.9.4" "8.9" "8"
+pushVersion "9.5.0" "9.5" "9"
+pushVersion "8.9.5" "8.9" "8"
