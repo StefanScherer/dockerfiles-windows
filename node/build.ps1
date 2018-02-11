ee9621e6fb80ac9c4ebd9bec41ef77cd10db972e
Stefan Scherer <scherer_stefan@icloud.com>
2018-02-11 18:23:57 +0100

    Build only 8 and 9

--- node/build.ps1
+++ node/build.ps1
@@ -8,6 +8,6 @@ function buildVersion($majorMinorPatch, $majorMinor, $major) {
   docker build -t node:$majorMinorPatch-nanoserver $majorMinor/nano
 }
 
-buildVersion "6.12.3" "6.12" "6"
+# buildVersion "6.12.3" "6.12" "6"
 buildVersion "9.5.0" "9.5" "9"
 buildVersion "8.9.4" "8.9" "8"
