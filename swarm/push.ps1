36f67804af857d2087f1c5b07da17305ebc6fb62
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-09 12:28:12 +0100

    Rebase swarm with netapi32.dll

--- swarm/push.ps1
+++ swarm/push.ps1
@@ -1,5 +1,25 @@
-docker tag swarm:1.2.8 stefanscherer/swarm-windows:1.2.8
-docker tag swarm:latest stefanscherer/swarm-windows:latest
+$version=$(select-string -Path Dockerfile -Pattern "ENV SWARM_VERSION").ToString().split()[-1]
 
-docker push stefanscherer/swarm-windows:1.2.8
-docker push stefanscherer/swarm-windows:latest
+docker tag swarm:$version stefanscherer/swarm-windows:$version-1607
+docker push stefanscherer/swarm-windows:$version-1607
+
+npm install -g rebase-docker-image
+
+rebase-docker-image stefanscherer/swarm-windows:$version-1607 `
+  -s microsoft/nanoserver:10.0.14393.2068 `
+  -t stefanscherer/swarm-windows:$version-1709 `
+  -b stefanscherer/netapi-helper:1709
+
+..\update-docker-cli.ps1
+
+docker manifest create `
+  stefanscherer/swarm-windows:$version `
+  stefanscherer/swarm-windows:$version-1607 `
+  stefanscherer/swarm-windows:$version-1709
+docker manifest push stefanscherer/swarm-windows:$version
+
+docker manifest create `
+  stefanscherer/swarm-windows:latest `
+  stefanscherer/swarm-windows:$version-1607 `
+  stefanscherer/swarm-windows:$version-1709
+docker manifest push stefanscherer/swarm-windows:latest
--- swarm/test.ps1
+++ swarm/test.ps1
@@ -1,2 +1,3 @@
-docker run swarm:1.2.8 --help
-docker run swarm:1.2.8 create
+$version=$(select-string -Path Dockerfile -Pattern "ENV SWARM_VERSION").ToString().split()[-1]
+docker run swarm:$version --help
+docker run swarm:$version create
