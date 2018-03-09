bbb54558fa1c74bec98a0d29281da5c04d677974
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-09 12:14:53 +0100

    Trigger prometheus build

--- prometheus/push.ps1
+++ prometheus/push.ps1
@@ -3,6 +3,7 @@ docker tag prometheus stefanscherer/prometheus-windows:$version-1607
 docker push stefanscherer/prometheus-windows:$version-1607
 
 npm install -g rebase-docker-image
+
 rebase-docker-image stefanscherer/prometheus-windows:$version-1607 `
   -s microsoft/nanoserver:10.0.14393.2068 `
   -t stefanscherer/prometheus-windows:$version-1709 `
