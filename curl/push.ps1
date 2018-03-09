114e293ec0249416059f60fd81a9061bf4faac87
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-09 09:53:46 +0100

    Use 1607 tag to have that image as first in manifest list

--- curl/push.ps1
+++ curl/push.ps1
@@ -1,20 +1,20 @@
 $version=$(select-string -Path Dockerfile -Pattern "ENV CURL_VERSION").ToString().split()[-1]
-docker tag curl stefanscherer/curl-windows:$version-2016
-docker push stefanscherer/curl-windows:$version-2016
+docker tag curl stefanscherer/curl-windows:$version-1607
+docker push stefanscherer/curl-windows:$version-1607
 
 npm install -g rebase-docker-image
-rebase-docker-image stefanscherer/curl-windows:$version-2016 -t stefanscherer/curl-windows:$version-1709 -b microsoft/nanoserver:1709
+rebase-docker-image stefanscherer/curl-windows:$version-1607 -t stefanscherer/curl-windows:$version-1709 -b microsoft/nanoserver:1709
 
 ..\update-docker-cli.ps1
 
 docker manifest create `
   stefanscherer/curl-windows:$version `
-  stefanscherer/curl-windows:$version-2016 `
+  stefanscherer/curl-windows:$version-1607 `
   stefanscherer/curl-windows:$version-1709
 docker manifest push stefanscherer/curl-windows:$version
 
 docker manifest create `
   stefanscherer/curl-windows:latest `
-  stefanscherer/curl-windows:$version-2016 `
+  stefanscherer/curl-windows:$version-1607 `
   stefanscherer/curl-windows:$version-1709
 docker manifest push stefanscherer/curl-windows:latest
