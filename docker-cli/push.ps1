f251e2156b39317ed27295e56f4aa59496530a36
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-09 09:54:13 +0100

    Use 1607 tag to have that image as first in manifest list

--- docker-cli/push.ps1
+++ docker-cli/push.ps1
@@ -1,10 +1,10 @@
 $version=$(select-string -Path Dockerfile -Pattern "ENV DOCKER_VERSION").ToString().split()[-1]
-docker tag docker-cli stefanscherer/docker-cli-windows:$version-2016
-docker push stefanscherer/docker-cli-windows:$version-2016
+docker tag docker-cli stefanscherer/docker-cli-windows:$version-1607
+docker push stefanscherer/docker-cli-windows:$version-1607
 
 npm install -g rebase-docker-image
 rebase-docker-image `
-  stefanscherer/docker-cli-windows:$version-2016 `
+  stefanscherer/docker-cli-windows:$version-1607 `
   -t stefanscherer/docker-cli-windows:$version-1709 `
   -b microsoft/nanoserver:1709_KB4074588
 
@@ -12,12 +12,12 @@ rebase-docker-image `
 
 docker manifest create `
   stefanscherer/docker-cli-windows:$version `
-  stefanscherer/docker-cli-windows:$version-2016 `
+  stefanscherer/docker-cli-windows:$version-1607 `
   stefanscherer/docker-cli-windows:$version-1709
 docker manifest push stefanscherer/docker-cli-windows:$version
 
 docker manifest create `
   stefanscherer/docker-cli-windows:latest `
-  stefanscherer/docker-cli-windows:$version-2016 `
+  stefanscherer/docker-cli-windows:$version-1607 `
   stefanscherer/docker-cli-windows:$version-1709
 docker manifest push stefanscherer/docker-cli-windows:latest
