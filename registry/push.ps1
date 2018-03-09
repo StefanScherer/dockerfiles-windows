b8fe401ba64881a792d4510f5a42b10c8d8a3606
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-09 09:54:57 +0100

    Use 1607 tag to have that image as first in manifest list

--- registry/push.ps1
+++ registry/push.ps1
@@ -1,20 +1,20 @@
 $version=$(select-string -Path Dockerfile -Pattern "ENV DISTRIBUTION_VERSION").ToString().split()[-1].SubString(1)
-docker tag registry:$version stefanscherer/registry-windows:$version-2016
-docker push stefanscherer/registry-windows:$version-2016
+docker tag registry:$version stefanscherer/registry-windows:$version-1607
+docker push stefanscherer/registry-windows:$version-1607
 
 npm install -g rebase-docker-image
-rebase-docker-image stefanscherer/registry-windows:$version-2016 -t stefanscherer/registry-windows:$version-1709 -b microsoft/nanoserver:1709
+rebase-docker-image stefanscherer/registry-windows:$version-1607 -t stefanscherer/registry-windows:$version-1709 -b microsoft/nanoserver:1709
 
-Invoke-WebRequest -UseBasicParsing https://6582-88013053-gh.circle-artifacts.com/0/work/build/docker-windows-amd64 -OutFile docker.exe
+..\update-docker-cli.ps1
 
-.\docker.exe manifest create `
+docker manifest create `
   stefanscherer/registry-windows:$version `
-  stefanscherer/registry-windows:$version-2016 `
+  stefanscherer/registry-windows:$version-1607 `
   stefanscherer/registry-windows:$version-1709
-.\docker.exe manifest push stefanscherer/registry-windows:$version
+docker manifest push stefanscherer/registry-windows:$version
 
-.\docker.exe manifest create `
+docker manifest create `
   stefanscherer/registry-windows:latest `
-  stefanscherer/registry-windows:$version-2016 `
+  stefanscherer/registry-windows:$version-1607 `
   stefanscherer/registry-windows:$version-1709
-.\docker.exe manifest push stefanscherer/registry-windows:latest
+docker manifest push stefanscherer/registry-windows:latest
