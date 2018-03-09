8d45659468fd1091de09a9ac1d1d17b42d145be4
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-09 09:56:13 +0100

    Use 1607 tag to have that image as first in manifest list

--- consul/push.ps1
+++ consul/push.ps1
@@ -1,10 +1,10 @@
 $version=$(select-string -Path Dockerfile -Pattern "ENV CONSUL_VERSION").ToString().split()[-1]
-docker tag consul stefanscherer/consul-windows:$version-2016
-docker push stefanscherer/consul-windows:$version-2016
+docker tag consul stefanscherer/consul-windows:$version-1607
+docker push stefanscherer/consul-windows:$version-1607
 
 npm install -g rebase-docker-image
 rebase-docker-image `
-  stefanscherer/consul-windows:$version-2016 `
+  stefanscherer/consul-windows:$version-1607 `
   -t stefanscherer/consul-windows:$version-1709 `
   -b microsoft/nanoserver:1709
 
@@ -12,12 +12,12 @@ rebase-docker-image `
 
 docker manifest create `
   stefanscherer/consul-windows:$version `
-  stefanscherer/consul-windows:$version-2016 `
+  stefanscherer/consul-windows:$version-1607 `
   stefanscherer/consul-windows:$version-1709
 docker manifest push stefanscherer/consul-windows:$version
 
 docker manifest create `
   stefanscherer/consul-windows:latest `
-  stefanscherer/consul-windows:$version-2016 `
+  stefanscherer/consul-windows:$version-1607 `
   stefanscherer/consul-windows:$version-1709
 docker manifest push stefanscherer/consul-windows:latest
