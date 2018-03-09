daa29201046b72411d8b62400cd40127564e4bc7
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-09 12:41:30 +0100

    Build Traefik 1709 image with netapi dll

--- traefik/push.ps1
+++ traefik/push.ps1
@@ -1,5 +1,25 @@
 $version=$(select-string -Path Dockerfile -Pattern "ENV TRAEFIK_VERSION").ToString().split()[-1]
-docker tag traefik stefanscherer/traefik-windows
-docker tag traefik stefanscherer/traefik-windows:v$version
-docker push stefanscherer/traefik-windows:v$version
-docker push stefanscherer/traefik-windows
+
+docker tag traefik stefanscherer/traefik-windows:v$version-1607
+docker push stefanscherer/traefik-windows:v$version-1607
+
+npm install -g rebase-docker-image
+
+rebase-docker-image stefanscherer/traefik-windows:v$version-1607 `
+  -s microsoft/nanoserver:10.0.14393.2068 `
+  -t stefanscherer/traefik-windows:v$version-1709 `
+  -b stefanscherer/netapi-helper:1709
+
+..\update-docker-cli.ps1
+
+docker manifest create `
+  stefanscherer/traefik-windows:v$version `
+  stefanscherer/traefik-windows:v$version-1607 `
+  stefanscherer/traefik-windows:v$version-1709
+docker manifest push stefanscherer/traefik-windows:v$version
+
+docker manifest create `
+  stefanscherer/traefik-windows:latest `
+  stefanscherer/traefik-windows:v$version-1607 `
+  stefanscherer/traefik-windows:v$version-1709
+docker manifest push stefanscherer/traefik-windows:latest
