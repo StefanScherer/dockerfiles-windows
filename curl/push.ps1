3f24c4a5c8d784033c84627dd0ca33b1d5c88c03
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-09 09:32:32 +0100

    Update Curl 7.58.0

--- curl/push.ps1
+++ curl/push.ps1
@@ -5,16 +5,16 @@ docker push stefanscherer/curl-windows:$version-2016
 npm install -g rebase-docker-image
 rebase-docker-image stefanscherer/curl-windows:$version-2016 -t stefanscherer/curl-windows:$version-1709 -b microsoft/nanoserver:1709
 
-Invoke-WebRequest -UseBasicParsing https://6582-88013053-gh.circle-artifacts.com/0/work/build/docker-windows-amd64 -OutFile docker.exe
+..\update-docker-cli.ps1
 
-.\docker.exe manifest create `
+docker manifest create `
   stefanscherer/curl-windows:$version `
   stefanscherer/curl-windows:$version-2016 `
   stefanscherer/curl-windows:$version-1709
-.\docker.exe manifest push stefanscherer/curl-windows:$version
+docker manifest push stefanscherer/curl-windows:$version
 
-.\docker.exe manifest create `
+docker manifest create `
   stefanscherer/curl-windows:latest `
   stefanscherer/curl-windows:$version-2016 `
   stefanscherer/curl-windows:$version-1709
-.\docker.exe manifest push stefanscherer/curl-windows:latest
+docker manifest push stefanscherer/curl-windows:latest
