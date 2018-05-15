a61a0f9f63ce493e162ae985ef63978ce94f89f9
Stefan Scherer <scherer_stefan@icloud.com>
2018-05-15 07:42:00 +0200

    Update base image, add 1803

--- traefik/push.ps1
+++ traefik/push.ps1
@@ -6,20 +6,27 @@ docker push stefanscherer/traefik-windows:v$version-1607
 npm install -g rebase-docker-image
 
 rebase-docker-image stefanscherer/traefik-windows:v$version-1607 `
-  -s microsoft/nanoserver:10.0.14393.2189 `
+  -s microsoft/nanoserver:10.0.14393.2248 `
   -t stefanscherer/traefik-windows:v$version-1709 `
   -b stefanscherer/netapi-helper:1709
+
+  rebase-docker-image stefanscherer/traefik-windows:v$version-1607 `
+    -s microsoft/nanoserver:10.0.14393.2248 `
+    -t stefanscherer/traefik-windows:v$version-1803 `
+    -b stefanscherer/netapi-helper:1803
 
 ..\update-docker-cli.ps1
 
 docker manifest create `
   stefanscherer/traefik-windows:v$version `
   stefanscherer/traefik-windows:v$version-1607 `
-  stefanscherer/traefik-windows:v$version-1709
+  stefanscherer/traefik-windows:v$version-1709 `
+  stefanscherer/traefik-windows:v$version-1803
 docker manifest push stefanscherer/traefik-windows:v$version
 
 docker manifest create `
   stefanscherer/traefik-windows:latest `
   stefanscherer/traefik-windows:v$version-1607 `
-  stefanscherer/traefik-windows:v$version-1709
+  stefanscherer/traefik-windows:v$version-1709 `
+  stefanscherer/traefik-windows:v$version-1803
 docker manifest push stefanscherer/traefik-windows:latest
