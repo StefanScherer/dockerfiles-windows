2b4f7770a65776e5a2a706f5ad6681440f080535
Stefan Scherer <scherer_stefan@icloud.com>
2018-06-01 17:20:35 +0200

    Show stderr

--- node/test.ps1
+++ node/test.ps1
@@ -1,11 +1,13 @@
 function testCommand($image, $command, $expected) {
   Write-Host "Testing: docker run $image $command --version"
   $ErrorActionPreference = 'SilentlyContinue';
-  $actual = $(docker run $image $command --version)
+  $allOutput = & docker run $image $command --version 2>&1
+  $stderr = $allOutput | ?{ $_ -is [System.Management.Automation.ErrorRecord] }
+  $actual = $allOutput | ?{ $_ -isnot [System.Management.Automation.ErrorRecord] }
   $ErrorActionPreference = 'Stop';
   Write-Host $actual
   if ($LastExitCode) {
-    Write-Error "Command exited with error $LastExitCode"
+    Write-Error "Command exited with error $LastExitCode: $stderr"
   }
   if ($expected -and ($actual -ne $expected)) {
     Write-Error "Expected '$expected', but got '$actual'"
@@ -19,12 +21,12 @@ function testVersion($majorMinorPatch) {
   testCommand "node:$majorMinorPatch-windowsservercore" "node" "v$majorMinorPatch"
   testCommand "node:$majorMinorPatch-windowsservercore" "npm.cmd" ""
   testCommand "node:$majorMinorPatch-windowsservercore" "yarn.cmd" "1.7.0"
-#  testCommand "node:$majorMinorPatch-windowsservercore" "git" "git version 2.17.1.windows.2"
+  testCommand "node:$majorMinorPatch-windowsservercore" "git" "git version 2.17.1.windows.2"
 
   testCommand "node:$majorMinorPatch-nanoserver" "node" "v$majorMinorPatch"
   testCommand "node:$majorMinorPatch-nanoserver" "npm.cmd" ""
   testCommand "node:$majorMinorPatch-nanoserver" "yarn.cmd" "1.7.0"
-#  testCommand "node:$majorMinorPatch-nanoserver" "git" "git version 2.17.1.windows.2"
+  testCommand "node:$majorMinorPatch-nanoserver" "git" "git version 2.17.1.windows.2"
 
   testCommand "node:$majorMinorPatch-pure" "node" "v$majorMinorPatch"
 }
