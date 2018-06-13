430971f2cfa1a17c6202df663f2dd35e6223317d
Stefan Scherer <scherer_stefan@icloud.com>
2018-06-13 19:56:06 +0200

    Build only node 8

--- node/push.ps1
+++ node/push.ps1
@@ -94,6 +94,6 @@ manifests:
 npm install -g rebase-docker-image
 choco install -y manifest-tool
 
-pushVersion "6.14.3" "6.14" "6"
+#pushVersion "6.14.3" "6.14" "6"
 pushVersion "8.11.3" "8.11" "8"
 #pushVersion "10.4.1" "10.4" "10"
