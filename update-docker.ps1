2028542e9cfda9c0d995687e1d4b31b38ded7b9e
Stefan Scherer <scherer_stefan@icloud.com>
2018-07-19 21:57:39 +0200

    Install Package provider

--- update-docker.ps1
+++ update-docker.ps1
@@ -1,6 +1,8 @@
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
+Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
 Install-Package -Name docker -ProviderName DockerMsftProvider -Verbose -Update -Force
 
 Write-Host "Starting docker service"
