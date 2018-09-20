ba46d54ab8e64c9a83777c98ba38ea001b062b5a
Stefan Scherer <scherer_stefan@icloud.com>
2018-09-20 16:17:05 +0200

    No need to update docker engine

--- node/build.ps1
+++ node/build.ps1
@@ -12,9 +12,6 @@ function buildVersion($majorMinorPatch, $majorMinor, $major) {
   docker build -t node:$majorMinorPatch-nanoserver $major/nano
 }
 
-Write-Output "Running update-docker.ps1"
-..\update-docker.ps1
-
 #buildVersion "6.14.4" "6.14" "6"
 #buildVersion "8.11.4" "8.11" "8"
 
