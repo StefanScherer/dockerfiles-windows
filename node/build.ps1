26e554cc8b40dec827c38812d66ad1c115a531bc
Stefan Scherer <scherer_stefan@icloud.com>
2018-09-21 18:16:54 +0200

    Fix version

--- node/build.ps1
+++ node/build.ps1
@@ -13,13 +13,13 @@ function buildVersion($majorMinorPatch, $majorMinor, $major) {
 }
 
 Write-Output "Build with server docker engine"
-$ErrorActionPreference = 'Continue';
-$docker_version = "18.03.1-ee-3"
+# $ErrorActionPreference = 'Continue'
+$docker_version = "18-03-1-ee-3"
 wget -outfile $env:TEMP\docker.zip $("https://dockermsft.blob.core.windows.net/dockercontainer/docker-{0}.zip" -f $docker_version)
 Expand-Archive -Path $env:TEMP\docker.zip -DestinationPath $env:TEMP -Force
 copy $env:TEMP\docker\*.exe $env:ProgramFiles\docker
 Remove-Item $env:TEMP\docker.zip
-$ErrorActionPreference = 'Stop';
+# $ErrorActionPreference = 'Stop'
 $env:PATH="c:\program files\docker;$env:PATH"
 Write-Output "Stop com.docker.service"
 Stop-Service com.docker.service
