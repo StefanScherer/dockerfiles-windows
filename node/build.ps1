508198d407ce614d01ce6147c5dbc212537aef8e
Stefan Scherer <scherer_stefan@icloud.com>
2018-09-21 13:45:11 +0200

    Test server docker engine

--- node/build.ps1
+++ node/build.ps1
@@ -12,6 +12,17 @@ function buildVersion($majorMinorPatch, $majorMinor, $major) {
   docker build -t node:$majorMinorPatch-nanoserver $major/nano
 }
 
+Write-Output "Test with server docker engine"
+curl.exe -Lo "C:\Program Files\docker\dockerd.exe" https://master.dockerproject.com/windows/x86_64/dockerd.exe
+$env:PATH="c:\program files\docker;$env:PATH"
+& sc stop com.docker.service
+Stop-Service docker
+dockerd --unregister-service
+dockerd --register-service  
+Start-Service docker
+Write-Output "Running server docker engine"
+docker version
+
 #buildVersion "6.14.4" "6.14" "6"
 #buildVersion "8.11.4" "8.11" "8"
 
