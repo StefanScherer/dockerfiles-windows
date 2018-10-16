29db60754c3f9834077e203ab1a99548dab1206f
Stefan Scherer <scherer_stefan@icloud.com>
2018-10-16 20:10:15 +0200

    Activate switching dockerd

--- node/build.ps1
+++ node/build.ps1
@@ -21,17 +21,17 @@ copy $env:TEMP\docker\*.exe $env:ProgramFiles\docker
 Remove-Item $env:TEMP\docker.zip
 # $ErrorActionPreference = 'Stop'
 $env:PATH="c:\program files\docker;$env:PATH"
-#Write-Output "Stop com.docker.service"
-#Stop-Service com.docker.service
-#Write-Output "Stop docker"
-#Stop-Service docker
-#Write-Output "Unregister docker"
-#dockerd --unregister-service
-#Write-Output "Register docker"
-#dockerd --register-service
-#Write-Output "Start docker"
-#Start-Service docker
-#Write-Output "Running server docker engine"
+Write-Output "Stop docker"
+Stop-Service docker
+Write-Output "Stop com.docker.service"
+Stop-Service com.docker.service
+Write-Output "Unregister docker"
+dockerd --unregister-service
+Write-Output "Register docker"
+dockerd --register-service
+Write-Output "Start docker"
+Start-Service docker
+Write-Output "Running server docker engine"
 docker version
 
 #buildVersion "6.14.4" "6.14" "6"
