4759a24a9b43a879cf874355922b276b3e32c04b
Stefan Scherer <scherer_stefan@icloud.com>
2018-12-06 22:19:42 +0100

    Update registry 2.7.0

--- registry/push.ps1
+++ registry/push.ps1
@@ -4,17 +4,23 @@ docker push stefanscherer/registry-windows:$version-1607
 
 npm install -g rebase-docker-image
 rebase-docker-image stefanscherer/registry-windows:$version-1607 -t stefanscherer/registry-windows:$version-1709 -b microsoft/nanoserver:1709
+rebase-docker-image stefanscherer/registry-windows:$version-1607 -t stefanscherer/registry-windows:$version-1803 -b microsoft/nanoserver:1803
+rebase-docker-image stefanscherer/registry-windows:$version-1607 -s microsoft/nanoserver:sac2016 -t stefanscherer/registry-windows:$version-1809 -b stefanscherer/nanoserver:10.0.17763.134
 
 ..\update-docker-cli.ps1
 
 docker manifest create `
   stefanscherer/registry-windows:$version `
   stefanscherer/registry-windows:$version-1607 `
-  stefanscherer/registry-windows:$version-1709
+  stefanscherer/registry-windows:$version-1709 `
+  stefanscherer/registry-windows:$version-1803 `
+  stefanscherer/registry-windows:$version-1809
 docker manifest push stefanscherer/registry-windows:$version
 
 docker manifest create `
   stefanscherer/registry-windows:latest `
   stefanscherer/registry-windows:$version-1607 `
-  stefanscherer/registry-windows:$version-1709
+  stefanscherer/registry-windows:$version-1709 `
+  stefanscherer/registry-windows:$version-1803 `
+  stefanscherer/registry-windows:$version-1809
 docker manifest push stefanscherer/registry-windows:latest
