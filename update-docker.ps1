69ef72148e41115bbaed1979767c912e2b8195bb
Stefan Scherer <scherer_stefan@icloud.com>
2018-12-09 13:17:26 +0100

    Fix update

--- update-docker.ps1
+++ update-docker.ps1
@@ -1,9 +1,10 @@
 Write-Output "Build with server docker engine"
 # $ErrorActionPreference = 'Continue'
+$ProgressPreference = 'SilentlyContinue'
 $docker_version = "18-09-0"
 wget -outfile $env:TEMP\docker.zip $("https://dockermsft.blob.core.windows.net/dockercontainer/docker-{0}.zip" -f $docker_version)
 Expand-Archive -Path $env:TEMP\docker.zip -DestinationPath $env:TEMP -Force
-copy $env:TEMP\docker\*.exe $env:ProgramFiles\docker
+copy $env:TEMP\docker\*.* $env:ProgramFiles\docker
 Remove-Item $env:TEMP\docker.zip
 # $ErrorActionPreference = 'Stop'
 $env:PATH="c:\program files\docker;$env:PATH"
