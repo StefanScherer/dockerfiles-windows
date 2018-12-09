98e74bf9b44368850ec63d2c87ef0e02bfca9a06
Stefan Scherer <scherer_stefan@icloud.com>
2018-12-09 12:15:20 +0100

    Update Docker EE

--- update-docker.ps1
+++ update-docker.ps1
@@ -1,11 +1,21 @@
-$ErrorActionPreference = 'Stop'
-$ProgressPreference = 'SilentlyContinue'
-
-Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
-Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
-Install-Package -Name docker -ProviderName DockerMsftProvider -Verbose -Update -Force
-
-Write-Host "Starting docker service"
+Write-Output "Build with server docker engine"
+# $ErrorActionPreference = 'Continue'
+$docker_version = "18-09-0"
+wget -outfile $env:TEMP\docker.zip $("https://dockermsft.blob.core.windows.net/dockercontainer/docker-{0}.zip" -f $docker_version)
+Expand-Archive -Path $env:TEMP\docker.zip -DestinationPath $env:TEMP -Force
+copy $env:TEMP\docker\*.exe $env:ProgramFiles\docker
+Remove-Item $env:TEMP\docker.zip
+# $ErrorActionPreference = 'Stop'
+$env:PATH="c:\program files\docker;$env:PATH"
+Write-Output "Stop docker"
+Stop-Service docker
+Write-Output "Stop com.docker.service"
+Stop-Service com.docker.service
+Write-Output "Unregister docker"
+dockerd --unregister-service
+Write-Output "Register docker"
+dockerd --register-service
+Write-Output "Start docker"
 Start-Service docker
-
+Write-Output "Running server docker engine"
 docker version
