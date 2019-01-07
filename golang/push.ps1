77151b697fabc8411e62a3bb7e2b0a7554a6b759
Stefan Scherer <scherer_stefan@icloud.com>
2019-01-07 08:12:52 +0100

    Update golang 1.11.4

--- golang/push.ps1
+++ golang/push.ps1
@@ -1,4 +1,4 @@
-docker tag golang stefanscherer/golang-windows:1.9
+docker tag golang stefanscherer/golang-windows:1.11.4
 docker tag golang stefanscherer/golang-windows
-docker push stefanscherer/golang-windows:1.9
+docker push stefanscherer/golang-windows:1.11.4
 docker push stefanscherer/golang-windows
