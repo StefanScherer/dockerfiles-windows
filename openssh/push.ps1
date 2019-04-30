83ca9c727fce445a98db802266e6e39a15a97de3
Stefan Scherer <stefan.scherer@docker.com>
2019-04-30 10:14:44 +0200

    Fix openssh push

--- openssh/push.ps1
+++ openssh/push.ps1
@@ -1,6 +1,6 @@
 docker tag openssh:1809 stefanscherer/openssh-windows:1809
 docker tag openssh:2016 stefanscherer/openssh-windows:2016
-docker push 1809 stefanscherer/openssh-windows:1809
+docker push stefanscherer/openssh-windows:1809
 docker push stefanscherer/openssh-windows:2016
 
 ..\update-docker-cli.ps1
