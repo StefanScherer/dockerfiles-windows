66779d9817afe83416a62fc9e53310485f67df63
Stefan Scherer <stefan.scherer@docker.com>
2019-05-01 23:10:22 +0200

    Rename image

--- winrm/push.ps1
+++ winrm/push.ps1
@@ -1,12 +1,12 @@
-docker tag winrm:1809 stefanscherer/winrm-windows:1809
-docker tag winrm:2016 stefanscherer/winrm-windows:2016
-docker push stefanscherer/winrm-windows:1809
-docker push stefanscherer/winrm-windows:2016
+docker tag winrm:1809 stefanscherer/winrm:1809
+docker tag winrm:2016 stefanscherer/winrm:2016
+docker push stefanscherer/winrm:1809
+docker push stefanscherer/winrm:2016
 
 ..\update-docker-cli.ps1
 
 docker manifest create `
-  stefanscherer/winrm-windows:latest `
-  stefanscherer/winrm-windows:2016 `
-  stefanscherer/winrm-windows:1809
-docker manifest push stefanscherer/winrm-windows:latest
+  stefanscherer/winrm:latest `
+  stefanscherer/winrm:2016 `
+  stefanscherer/winrm:1809
+docker manifest push stefanscherer/winrm:latest
