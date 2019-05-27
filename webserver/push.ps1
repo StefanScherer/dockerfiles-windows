1d945e29977b42ae85e201f627136ab5884e0ea8
Stefan Scherer <stefan.scherer@docker.com>
2019-05-27 12:35:26 +0200

    Add 1903 image to manifest list

--- webserver/push.ps1
+++ webserver/push.ps1
@@ -1,4 +1,4 @@
-$version="0.3.0"
+$version="0.4.0"
 
 docker tag webserver stefanscherer/webserver-windows:$version-1607-deprecated
 docker push stefanscherer/webserver-windows:$version-1607-deprecated
@@ -15,16 +15,23 @@ rebase-docker-image stefanscherer/webserver-windows:$version-1607-deprecated `
   -t stefanscherer/webserver-windows:$version-1809 `
   -b mcr.microsoft.com/windows/nanoserver:1809
 
+rebase-docker-image stefanscherer/webserver-windows:$version-1607-deprecated `
+  -s mcr.microsoft.com/windows/nanoserver:sac2016 `
+  -t stefanscherer/webserver-windows:$version-1903 `
+  -b mcr.microsoft.com/windows/nanoserver:1903
+
 ..\update-docker-cli.ps1
 
 docker manifest create `
   stefanscherer/webserver-windows:$version `
   stefanscherer/webserver-windows:$version-1803 `
-  stefanscherer/webserver-windows:$version-1809
+  stefanscherer/webserver-windows:$version-1809 `
+  stefanscherer/webserver-windows:$version-1903
 docker manifest push stefanscherer/webserver-windows:$version
 
 docker manifest create `
   stefanscherer/webserver-windows:latest `
   stefanscherer/webserver-windows:$version-1803 `
-  stefanscherer/webserver-windows:$version-1809
+  stefanscherer/webserver-windows:$version-1809 `
+  stefanscherer/webserver-windows:$version-1903
 docker manifest push stefanscherer/webserver-windows:latest
