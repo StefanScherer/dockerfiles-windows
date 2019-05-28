181e394e5d8557ac5ced8a83c246d3ef6063df4f
Stefan Scherer <stefan.scherer@docker.com>
2019-05-28 18:16:04 +0200

    Build 1903 helper

--- netapi-helper/build.sh
+++ netapi-helper/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$1" == "" ]; then
-  echo "Usage: $0 1803|1809"
+  echo "Usage: $0 1803|1809|1903"
   exit 1
 fi
 
 docker pull mcr.microsoft.com/windows/servercore:$1
 docker pull mcr.microsoft.com/windows/nanoserver:$1
-docker build -t stefanscherer/netapi-helper:$1 -f Dockerfile.$1 .
+docker build -t stefanscherer/netapi-helper:$1 -f Dockerfile.$1 --build-arg tag=$1 .
 docker push stefanscherer/netapi-helper:$1
