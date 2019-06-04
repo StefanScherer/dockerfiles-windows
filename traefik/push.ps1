7ad8cfb999df66a1af3197b4190704ae588b4e69
Stefan Scherer <stefan.scherer@docker.com>
2019-06-04 18:49:45 +0200

    Revert "Build 1903 variant"
    This reverts commit 3493e8b25c67b14caf753a6dcc632a111a832cd7.

--- traefik/push.ps1
+++ traefik/push.ps1
@@ -5,26 +5,16 @@ docker push stefanscherer/traefik-windows:v$version-1809
 docker tag traefik:1803 stefanscherer/traefik-windows:v$version-1803
 docker push stefanscherer/traefik-windows:v$version-1803
 
-npm install -g rebase-docker-image
-
-rebase-docker-image `
-  stefanscherer/traefik-windows:v$version-1809 `
-  -s mcr.microsoft.com/windows/nanoserver:1809 `
-  -t stefanscherer/traefik-windows:v$version-1903 `
-  -b mcr.microsoft.com/windows/nanoserver:1903
-
 ..\update-docker-cli.ps1
 
 docker manifest create `
   stefanscherer/traefik-windows:v$version `
   stefanscherer/traefik-windows:v$version-1803 `
-  stefanscherer/traefik-windows:v$version-1809 `
-  stefanscherer/traefik-windows:v$version-1903
+  stefanscherer/traefik-windows:v$version-1809
 docker manifest push stefanscherer/traefik-windows:v$version
 
 docker manifest create `
   stefanscherer/traefik-windows:latest `
   stefanscherer/traefik-windows:v$version-1803 `
-  stefanscherer/traefik-windows:v$version-1809 `
-  stefanscherer/traefik-windows:v$version-1903
+  stefanscherer/traefik-windows:v$version-1809
 docker manifest push stefanscherer/traefik-windows:latest
