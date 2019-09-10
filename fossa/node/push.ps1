4e5cab1ae30a291e39fc2ca4ca592dd9cacdd358
Stefan Scherer <stefan.scherer@docker.com>
2019-09-10 17:23:17 +0200

    Add FOSSA + Node.js

---
+++ fossa/node/push.ps1
@@ -0,0 +1,6 @@
+$fossa_version=$(select-string -Path Dockerfile -Pattern "ENV FOSSA_VERSION").ToString().split()[-1]
+$node_version=$(select-string -Path Dockerfile -Pattern "ENV NODE_VERSION").ToString().split()[-1]
+docker tag fossa-node stefanscherer/fossa-node-windows:$fossa_version-$node_version
+docker push stefanscherer/fossa-node-windows:$fossa_version-$node_version
+docker tag fossa-node stefanscherer/fossa-node-windows:latest
+docker push stefanscherer/fossa-node-windows:latest
---
+++ fossa/node/test.ps1
@@ -0,0 +1,2 @@
+docker run fossa-node fossa --version
+docker run fossa-node node --version
