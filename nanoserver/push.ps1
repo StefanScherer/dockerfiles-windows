8f7611d0b89773ece22bb9d5ba42fe7880f226a5
Stefan Scherer <stefan.scherer@docker.com>
2019-09-14 20:04:20 +0200

    Build nanoserver

---
+++ nanoserver/push.ps1
@@ -0,0 +1,12 @@
+$image="mcr.microsoft.com/windows/nanoserver"
+
+Function PullTagPush($image, $tag) {
+  $osversion=$(docker image inspect "$image:$tag" | jq -r '.[0].OsVersion')
+  echo "Pushing stefanscherer/nanoserver:$tag"
+  docker push "stefanscherer/nanoserver:$tag"
+  echo "Pushing stefanscherer/nanoserver:$osversion"
+  docker push "stefanscherer/nanoserver:$osversion"
+}
+
+Push($image, "1809")
+Push($image, "1803")
