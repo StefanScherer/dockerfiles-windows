b472fec6db0470a020fbe9bba0024f40141e7fa5
Stefan Scherer <stefan.scherer@docker.com>
2019-09-14 20:22:58 +0200

    Fix vars

--- nanoserver/build.ps1
+++ nanoserver/build.ps1
@@ -1,10 +1,10 @@
 $image="mcr.microsoft.com/windows/nanoserver:1809"
 
 Function PullTagPush($image, $tag) {
-  docker pull "$image:$tag"
-  $osversion=$(docker image inspect "$image:$tag" | jq -r '.[0].OsVersion')
-  docker tag "$image:$tag" "stefanscherer/nanoserver:$tag"
-  docker tag "$image:$tag" "stefanscherer/nanoserver:$osversion"
+  docker pull "${image}:${tag}"
+  $osversion=$(docker image inspect "${image}:${tag}" | jq -r '.[0].OsVersion')
+  docker tag "${image}:${tag}" "stefanscherer/nanoserver:$tag"
+  docker tag "${image}:${tag}" "stefanscherer/nanoserver:$osversion"
 }
 
 PullTagPush($image, "1809")
--- nanoserver/push.ps1
+++ nanoserver/push.ps1
@@ -1,7 +1,7 @@
 $image="mcr.microsoft.com/windows/nanoserver"
 
 Function PullTagPush($image, $tag) {
-  $osversion=$(docker image inspect "$image:$tag" | jq -r '.[0].OsVersion')
+  $osversion=$(docker image inspect "${image}:${tag}" | jq -r '.[0].OsVersion')
   echo "Pushing stefanscherer/nanoserver:$tag"
   docker push "stefanscherer/nanoserver:$tag"
   echo "Pushing stefanscherer/nanoserver:$osversion"
