712a7dea260772c97e89a7a3f034bed830d9a112
Stefan Scherer <stefan.scherer@docker.com>
2019-09-14 20:30:15 +0200

    Fix image name

--- nanoserver/build.ps1
+++ nanoserver/build.ps1
@@ -1,6 +1,7 @@
-$image="mcr.microsoft.com/windows/nanoserver:1809"
+$image="mcr.microsoft.com/windows/nanoserver"
 
 Function PullTagPush($image, $tag) {
+  Write-Output "Pulling ${image}:${tag}"
   docker pull "${image}:${tag}"
   $osversion=$(docker image inspect "${image}:${tag}" | jq -r '.[0].OsVersion')
   docker tag "${image}:${tag}" "stefanscherer/nanoserver:$tag"
