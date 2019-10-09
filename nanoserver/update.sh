8d77f0597f6dbae4eb691e206210710383307584
Stefan Scherer <stefan.scherer@docker.com>
2019-10-09 07:05:07 +0200

    ./update.sh [1803|1809|1903]

--- nanoserver/update.sh
+++ nanoserver/update.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-image=mcr.microsoft.com/windows/nanoserver:1809
+osversion=${1:-1809}
+echo $osversion
+image=mcr.microsoft.com/windows/nanoserver:$osversion
 docker pull $image
 tag=$(docker image inspect $image | jq -r '.[0].OsVersion')
 docker tag $image stefanscherer/nanoserver:$tag
 echo "Pushing stefanscherer/nanoserver:$tag"
 docker push stefanscherer/nanoserver:$tag
+docker tag $image stefanscherer/nanoserver:$osversion
+echo "Pushing stefanscherer/nanoserver:$osversion"
+docker push stefanscherer/nanoserver:$osversion
+
