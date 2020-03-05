5fae7a3e7b359112fee9ef9deb3d5d56c265495a
Stefan Scherer <stefan.scherer@docker.com>
2020-03-05 15:39:04 +0100

    Revert update

--- node/build.ps1
+++ node/build.ps1
@@ -14,7 +14,7 @@ function buildVersion($majorMinorPatch, $majorMinor, $major) {
 
 Write-Output "Build with server docker engine"
 # $ErrorActionPreference = 'Continue'
-$docker_version = "19-03-5"
+$docker_version = "18-09-6"
 wget -outfile $env:TEMP\docker.zip $("https://dockermsft.blob.core.windows.net/dockercontainer/docker-{0}.zip" -f $docker_version)
 Expand-Archive -Path $env:TEMP\docker.zip -DestinationPath $env:TEMP -Force
 copy $env:TEMP\docker\*.* $env:ProgramFiles\docker
